8c966027a40fd494cc6735a74b12c3d85863dd9d
haru <hrtkszk@gmail.com>
2022-04-30 13:07:50 +0900

    change entirely  authentication

--- src/App.js
+++ src/App.js
@@ -126,46 +126,216 @@
 // }
 
 
+// import {
+//   Routes,
+//   Route,
+//   Link,
+//   Outlet,
+// } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route path="invoices" element={<Invoices />} />
+//         <Route path="dashboard" element={<Dashboard />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
+
+// function Layout() {
+//   return (
+//     <div>
+//       <h1>Welcome to the app!</h1>
+//       <nav>
+//         <Link to="invoices">Invoices</Link> |{" "}
+//         <Link to="dashboard">Dashboard</Link>
+//       </nav>
+//       <div className="content">
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Invoices() {
+//   return <h1>Invoices</h1>;
+// }
+
+// function Dashboard() {
+//   return <h1>Dashboard</h1>;
+// }
+// export default App;
+
+
+import * as React from "react";
 import {
   Routes,
   Route,
   Link,
+  useNavigate,
+  useLocation,
+  Navigate,
   Outlet,
 } from "react-router-dom";
+import { fakeAuthProvider } from "./auth";
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="invoices" element={<Invoices />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <h1>Auth Example</h1>
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <ProtectedPage />
+              </RequireAuth>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
 function Layout() {
   return (
     <div>
-      <h1>Welcome to the app!</h1>
-      <nav>
-        <Link to="invoices">Invoices</Link> |{" "}
-        <Link to="dashboard">Dashboard</Link>
-      </nav>
-      <div className="content">
-        <Outlet />
-      </div>
+      <AuthStatus />
+
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
+
+      <Outlet />
     </div>
   );
 }
 
-function Invoices() {
-  return <h1>Invoices</h1>;
+// interface AuthContextType {
+//   user: any;
+//   signin: (user: string, callback: VoidFunction) => void;
+//   signout: (callback: VoidFunction) => void;
+// }
+
+let AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState<any>(null);
+
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  let value = { user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
-function Dashboard() {
-  return <h1>Dashboard</h1>;
+function useAuth() {
+  return React.useContext(AuthContext);
 }
-export default App;
 
+function AuthStatus() {
+  let auth = useAuth();
+  let navigate = useNavigate();
 
+  if (!auth.user) {
+    return <p>You are not logged in.</p>;
+  }
+
+  return (
+    <p>
+      Welcome {auth.user}!{" "}
+      <button
+        onClick={() => {
+          auth.signout(() => navigate("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  );
+}
+
+function RequireAuth({ children }) {
+  let auth = useAuth();
+  let location = useLocation();
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+function LoginPage() {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let auth = useAuth();
+
+  let from = location.state?.from?.pathname || "/";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+    let username = formData.get("username");
+
+    auth.signin(username, () => {
+      // Send them back to the page they tried to visit when they were
+      // redirected to the login page. Use { replace: true } so we don't create
+      // another entry in the history stack for the login page.  This means that
+      // when they get to the protected page and click the back button, they
+      // won't end up back on the login page, which is also really nice for the
+      // user experience.
+      navigate(from, { replace: true });
+    });
+  }
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from}</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username: <input name="username" type="text" />
+        </label>{" "}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
