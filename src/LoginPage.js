3861556e93864b3f48da3d5225538931d09e7909
haru <hrtkszk@gmail.com>
2022-04-30 14:19:01 +0900

    update login redirection for empty username

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -20,17 +20,19 @@ export function LoginPage() {
     let username = formData.get("username");
     console.log(username)
 
-    if (username === "") {navigate("/", {replace:true})}
-
-    auth.signin(username, () => {
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
-      navigate(from, { replace: true });
-    });
+    if (username === "") {
+        navigate("/", {replace:true})
+    } else {
+        auth.signin(username, () => {
+        // Send them back to the page they tried to visit when they were
+        // redirected to the login page. Use { replace: true } so we don't create
+        // another entry in the history stack for the login page.  This means that
+        // when they get to the protected page and click the back button, they
+        // won't end up back on the login page, which is also really nice for the
+        // user experience.
+        navigate(from, { replace: true });
+        });
+    }
   }
 
   return (
