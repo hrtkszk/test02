0c94690875c7f084dbadea19963165ce309962de
haru <hrtkszk@gmail.com>
2022-05-02 15:00:59 +0900

    update send_post.php just to exe post2SQL.py

--- public/send_post.php
+++ public/send_post.php
@@ -1,50 +1,51 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
-$rest_json = file_get_contents("php://input"); // JSONでPOSTされたデータを取り出す
-$_POST = json_decode($rest_json, true); // JSON文字列をデコード
-$command="python3 post2SQL.py ".$_POST['id']; //pythonに引数を渡す
-exec($command,$output); //python実行と、返り数受け取り
+exec("python3 post2SQL.py")
+// $rest_json = file_get_contents("php://input"); // JSONでPOSTされたデータを取り出す
+// $_POST = json_decode($rest_json, true); // JSON文字列をデコード
+// $command="python3 post2SQL.py ".$_POST['id','message']; //pythonに引数を渡す
+// exec($command,$output); //python実行と、返り数受け取り
 
-// pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
-$output[0]=trim($output[0],"\"['");
-$output[0]=trim($output[0],"']\"");
-$output0=explode("', '",$output[0]);
+// // pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
+// $output[0]=trim($output[0],"\"['");
+// $output[0]=trim($output[0],"']\"");
+// $output0=explode("', '",$output[0]);
 
-$output2=array();
-$it = 0;
+// $output2=array();
+// $it = 0;
 
-// pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
-foreach ($output as $value) {
-    $value=trim($value,"\"[");
-    $value=trim($value,"]\"");
-    $value=str_replace("'",'',$value);
-    $value1=explode(", ",$value);
-    $output1 = array_combine($output0,$value1);
-    $output2 = $output2 + array("$it" => $output1);
-    $it = $it + 1;
-}
-// 配列1行目の削除と、配列詰め
-unset($output2[0]);
-$output2 = array_values($output2);
+// // pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
+// foreach ($output as $value) {
+//     $value=trim($value,"\"[");
+//     $value=trim($value,"]\"");
+//     $value=str_replace("'",'',$value);
+//     $value1=explode(", ",$value);
+//     $output1 = array_combine($output0,$value1);
+//     $output2 = $output2 + array("$it" => $output1);
+//     $it = $it + 1;
+// }
+// // 配列1行目の削除と、配列詰め
+// unset($output2[0]);
+// $output2 = array_values($output2);
 
-//配列のJSON変換と、echoでのサーバーサイド出力。
-if(empty($_POST['id'])) {
-    echo json_encode(
-        [
-           "error" => true,
-           "message" => "Error: 入力してください。",
-           "pythonout2" => $output2,
-           "command" => $command,
-        ]
-    ); 
-} else {
-    echo json_encode(
-        [
-           "error" => false,
-           "message" => 'Success: 入力されたテキスト→'.$_POST['id'],
-           "pythonout2" => $output2,
-           "command" => $command,
-        ]
-    ); 
-}
+// //配列のJSON変換と、echoでのサーバーサイド出力。
+// if(empty($_POST['id'])) {
+//     echo json_encode(
+//         [
+//            "error" => true,
+//            "message" => "Error: 入力してください。",
+//            "pythonout2" => $output2,
+//            "command" => $command,
+//         ]
+//     ); 
+// } else {
+//     echo json_encode(
+//         [
+//            "error" => false,
+//            "message" => 'Success: 入力されたテキスト→'.$_POST['id'],
+//            "pythonout2" => $output2,
+//            "command" => $command,
+//         ]
+//     ); 
+// }
