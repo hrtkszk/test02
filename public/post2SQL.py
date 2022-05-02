7dfe8c53ed1b4a9fa9128bc3a9217e8cf6493025
haru <hrtkszk@gmail.com>
2022-05-02 14:40:11 +0900

    update post2SQL.py

--- public/post2SQL.py
+++ public/post2SQL.py
@@ -18,27 +18,29 @@ cursor = connection.cursor()
 
 cursor.execute(f"INSERT `LAA1422834-test`.`test` (`ID`, `DateTime`, `message`) VALUES ('{sys.argv[1]}', CURRENT_TIMESTAMP, '{sys.argv[2]}')")
 
+
+
 #ここから下はreceive_get.phpで流してもよさそう
-cursor.execute(f"SELECT * FROM test WHERE id='{sys.argv[1]}'")
+#cursor.execute(f"SELECT * FROM test WHERE id='{sys.argv[1]}'")
 
 # num_fields = len(cursor.description)
-field_names = [i[0] for i in cursor.description]
-print(field_names)
+#field_names = [i[0] for i in cursor.description]
+# print(field_names)
 
-for row in cursor:
-    row1 = list()
-    for item in row:
-        if str(type(item)) == "<class 'datetime.datetime'>":
-            # 時間を文字列に変換（php側の処理対策）
-            row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
-        elif str(type(item)) == "<class 'str'>":
-            # phpでの文字列から配列への変換時の誤動作防止用前処理
-            item = item.replace("'","’")
-            row1.append(item.replace(', ', '，'))
-        else:
-            row1.append(item)
-    # printでのpythonからphpへの受け渡し
-    print (row1)
+# for row in cursor:
+#     row1 = list()
+#     for item in row:
+#         if str(type(item)) == "<class 'datetime.datetime'>":
+#             # 時間を文字列に変換（php側の処理対策）
+#             row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
+#         elif str(type(item)) == "<class 'str'>":
+#             # phpでの文字列から配列への変換時の誤動作防止用前処理
+#             item = item.replace("'","’")
+#             row1.append(item.replace(', ', '，'))
+#         else:
+#             row1.append(item)
+#     # printでのpythonからphpへの受け渡し
+#     print (row1)
 
 # 保存を実行
 connection.commit()
