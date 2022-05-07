4beab0d9810a4ad908fb857f571d696b1ed2857d
haru <hrtkszk@gmail.com>
2022-05-07 11:09:08 +0900

    update messagelist.py for readability

--- public/messageList.py
+++ public/messageList.py
@@ -27,11 +27,11 @@ cursor.execute(f" \
     SELECT t1.* \
     FROM {table_name} AS t1 \
     JOIN ( \
-    SELECT ID, aiteID, MAX(messagedDateTime) AS latestDateTime \
-    FROM {table_name} \
-    WHERE ID='{sys.argv[1]}' \
-    GROUP BY aiteID) AS t2 \
-    ON t1.ID = t2.ID AND t1.aiteID = t2.aiteID AND t1.messagedDateTime = t2.latestDateTime \
+        SELECT ID, aiteID, MAX(messagedDateTime) AS latestDateTime \
+        FROM {table_name} \
+        WHERE ID='{sys.argv[1]}' \
+        GROUP BY aiteID) AS t2 \
+        ON t1.ID = t2.ID AND t1.aiteID = t2.aiteID AND t1.messagedDateTime = t2.latestDateTime \
     ORDER BY t1.messagedDateTime DESC\
 ")
 
