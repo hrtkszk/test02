c113616943762e9c519162785e4b26e2c36e15c6
haru <hrtkszk@gmail.com>
2022-05-08 11:36:38 +0900

    try deleting onChange for input func check

--- src/NewUser.js
+++ src/NewUser.js
@@ -54,25 +54,25 @@ export function NewUser() {
           name="email"
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
           title="アットマーク（“@”）を含んだ形式で入力してください"
-          onChange={evt => {
-            // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
-            setEmailPhone(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
-          }}
+          // onChange={evt => {
+          //   // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
+          //   setEmailPhone(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
+          // }}
           required
         /><br />
         <input
           type="password"
           name="password"
           pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-          onChange={evt => {
-            // パスワード入力がないとエラーとしたい
-            setPwd(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
-          }}
+          // onChange={evt => {
+          //   // パスワード入力がないとエラーとしたい
+          //   setPwd(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
+          // }}
           placeholder='パスワード'
           required
         /><br />
         <div>8文字以上で1文字以上の数字、小文字アルファベット、大文字アルファベットがそれぞれ含まれていること</div><br /><br />
-        <button onClick={setEmailPhonePwd}>登録する</button>
+        <button onClick={setEmailPhonePwd}>登録する</button><br />
         <Link to="../">戻る</Link>
       </div>
     </div>
