f146b8144949e43e51471706fda4364bc235130e
haru <hrtkszk@gmail.com>
2022-05-08 11:12:33 +0900

    add input limitation to newuser

--- src/NewUser.js
+++ src/NewUser.js
@@ -52,19 +52,23 @@ export function NewUser() {
           <footer>
             <input
               id="EmailPhone"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
               onChange={evt => {
                 // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
                 setEmailPhone(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
               }}
               placeholder='メールアドレス／電話番号'
-            />
+            /><br />
             <input
               id="Password"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
               onChange={evt => {
+                // パスワード入力がないとエラーとしたい
                 setPwd(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
               }}
               placeholder='パスワード'
-            />
+            /><br />
+            <div>8文字以上で1文字以上の数字、小文字アルファベット、大文字アルファベットがそれぞれ含まれていること</div><br /><br />
             <button onClick={setEmailPhonePwd}>登録する</button>
           </footer>
           <Link to="../">戻る</Link>
