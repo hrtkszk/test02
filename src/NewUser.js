f56f6f39cd7bf00014458225f3a8c07ec2b170a0
haru <hrtkszk@gmail.com>
2022-05-08 11:29:50 +0900

    update newuser.js

--- src/NewUser.js
+++ src/NewUser.js
@@ -49,31 +49,31 @@ export function NewUser() {
       <h1>新規登録</h1>
 
       <div>
-          <footer>
-            <input
-              id="EmailPhone"
-              type="email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-              onChange={evt => {
-                // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
-                setEmailPhone(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
-              }}
-              placeholder='メールアドレス／電話番号'
-            /><br />
-            <input
-              id="Password"
-              type="password"
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-              onChange={evt => {
-                // パスワード入力がないとエラーとしたい
-                setPwd(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
-              }}
-              placeholder='パスワード'
-            /><br />
-            <div>8文字以上で1文字以上の数字、小文字アルファベット、大文字アルファベットがそれぞれ含まれていること</div><br /><br />
-            <button onClick={setEmailPhonePwd}>登録する</button>
-          </footer>
-          <Link to="../">戻る</Link>
+        <input
+          type="email"
+          name="email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+          title="アットマーク（“@”）を含んだ形式で入力してください"
+          onChange={evt => {
+            // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
+            setEmailPhone(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
+          }}
+          required
+        /><br />
+        <input
+          type="password"
+          name="password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          onChange={evt => {
+            // パスワード入力がないとエラーとしたい
+            setPwd(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
+          }}
+          placeholder='パスワード'
+          required
+        /><br />
+        <div>8文字以上で1文字以上の数字、小文字アルファベット、大文字アルファベットがそれぞれ含まれていること</div><br /><br />
+        <button onClick={setEmailPhonePwd}>登録する</button>
+        <Link to="../">戻る</Link>
       </div>
     </div>
   )
