750d2492d7e4dfa34b387ef9517c71246c502ec4
haru <hrtkszk@gmail.com>
2022-05-14 16:14:40 +0900

    update checkaddmailpw.py 6

--- public/check_add_mailpw.py
+++ public/check_add_mailpw.py
@@ -42,7 +42,7 @@ for row in cursor:
 
 cursor.execute(f"INSERT `{SQLconfig.db}`.`{emailtable}` (`UUID`, `email`, `datetime`) VALUES (UUID(), '{sys.argv[1]}', CURRENT_TIME)")
 cursor.execute(f"SELECT * FROM {emailtable} WHERE email='{sys.argv[1]}'")
-print (cursor[0])
+print (cursor[0][1])
 field_names = [i[0] for i in cursor.description]
 print(field_names)
 for row in cursor:
