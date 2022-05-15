365a8a944b3a19ad24d5e59faba58e28fc7abe51
haru <hrtkszk@gmail.com>
2022-05-15 11:20:07 +0900

    update newuser and checkaddmailpw.py

--- public/check_add_mailpw.py
+++ public/check_add_mailpw.py
@@ -16,8 +16,6 @@ connection = MySQLdb.connect(
 emailtable="EmailSettings"
 pwdtable="PwdSettings"
 
-
-
 # メールアドレスが存在するかチェック。存在しなければ、アドレス・PW・UUID追加に進む。存在したら、同じUUIDでパスワードを追加する。
 
 # field name込みの場合はこっちを使う
@@ -25,35 +23,19 @@ pwdtable="PwdSettings"
 cursor = connection.cursor()
 
 cursor.execute(f"SELECT * FROM {emailtable} WHERE email='{sys.argv[1]}'")
-# field_names = [i[0] for i in cursor.description]
-# print(field_names)
-for row in cursor:
-    row1 = list()
-    for item in row:
-        row1.append(item)
-    # printでのpythonからphpへの受け渡し
-    print (row1)
 
-print(cursor.fetchall())
-if not(cursor.fetchall()):
-    print("cursor is null")
+checkExist = cursor.fetchall()
+if not(checkExist):
+    # UUID、メールアドレス登録→パスワード登録
+    # UUID、メールアドレス登録
+    cursor.execute(f"INSERT `{SQLconfig.db}`.`{emailtable}` (`UUID`, `email`, `datetime`) VALUES (UUID(), '{sys.argv[1]}', CURRENT_TIME)")
+    cursor.execute(f"SELECT UUID FROM {emailtable} WHERE email='{sys.argv[1]}'")
+    # パスワード登録
+    cursor.execute(f"INSERT `{SQLconfig.db}`.`{pwdtable}` (`UUID`, `password`, `datetime`) VALUES ('{cursor.fetchone()[0]}', '{sys.argv[2]}', CURRENT_TIME)")
+    print("Temporary Registration Complete")
 
-
-# UUID、メールアドレス登録→パスワード登録
-# UUID、メールアドレス登録
-
-
-cursor.execute(f"INSERT `{SQLconfig.db}`.`{emailtable}` (`UUID`, `email`, `datetime`) VALUES (UUID(), '{sys.argv[1]}', CURRENT_TIME)")
-cursor.execute(f"SELECT UUID FROM {emailtable} WHERE email='{sys.argv[1]}'")
-field_names = [i[0] for i in cursor.description]
-print(field_names)
-for row in cursor:
-    row1 = list()
-    for item in row:
-        row1.append(item)
-    # printでのpythonからphpへの受け渡し
-    print (row1)
-#
+else:
+    print("Email Exist")
 
 # 保存を実行
 connection.commit()
