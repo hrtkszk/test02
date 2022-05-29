5735b030e6a05205ff49bd355c67b709106734e2
haru <hrtkszk@gmail.com>
2022-05-29 15:49:55 +0900

    update registration.js to check gender select

--- src/Registration.js
+++ src/Registration.js
@@ -43,7 +43,7 @@ export function Registration(){
       <h1>登録が完了しました。</h1>
       <p>パスパラメーター：{userId}</p>
       <div>登録結果：{RegResult}</div>
-      <Link to="../../">戻る</Link>  
+
       <div>
           <form onSubmit={e => submit(e)}>
             <input
@@ -57,29 +57,26 @@ export function Registration(){
               placeholder='ニックネーム'
               required
             /><br />
-            <div onChange={evt => {setGender(evt.target.value)}}>
+            <div onChange={evt => {setGender(evt.target.value), console.log(Gender)}}>
               <input type="radio" value="Male" name="gender" /> 男性
               <input type="radio" value="Female" name="gender" /> 女性
               <input type="radio" value="Other" name="gender" /> LGBTQ
-            </div>
-            <br />
-
-
-              <input
-              type='number'
-              // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-              // 特殊文字を弾く必要がある。
-              // title="8文字以上で、数字・小文字アルファベット・大文字アルファベットを含めてください"
-              onChange={evt => {
-                setAge(evt.target.value)
-              }}
-              placeholder='年齢'
-              required
+            </div><br />
+            <input
+            type='number'
+            // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            // 特殊文字を弾く必要がある。
+            // title="8文字以上で、数字・小文字アルファベット・大文字アルファベットを含めてください"
+            onChange={evt => {
+              setAge(evt.target.value)
+            }}
+            placeholder='年齢'
+            required
             /><br />
             <button onClick={Register}>登録する</button>
           </form>
           <br />
-          <Link to="../">戻る</Link>
+          <Link to="../../">戻る</Link>  
         </div>
       </div>
   )
