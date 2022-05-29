7f2ff080722c6fde5e43dd353ea0e1bf5ce677fd
haru <hrtkszk@gmail.com>
2022-05-29 14:20:56 +0900

    update registaration.js

--- src/Registration.js
+++ src/Registration.js
@@ -7,27 +7,32 @@ import { useState } from 'react';
 
 export function Registration(){
   const [RegResult, setRegResult] = useState("");
+  const [initialized, setinitialized] = useState(false);
   let { userId } = useParams();
 
-  const Register = () => {
-    // 登録
-    const requestOptions1 ={
-      method: 'POST',
-      headers:{'Content-Type': 'application/json'},
-      body: JSON.stringify({"UUID":userId})
+  if (initialized===false) {
+    const Register = () => {
+      // 登録
+      const requestOptions1 ={
+        method: 'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({"UUID":userId})
+      }
+      fetch("../register.php",requestOptions1)
+      .then((response)=> response.json())
+      .then(result =>{
+        setRegResult(result)
+        console.log(result)
+      })
     }
-    fetch("../register.php",requestOptions1)
-    .then((response)=> response.json())
-    .then(result =>{
-      setRegResult(result)
-    })
+    setinitialized(true)
   }
 
   return(
     <div>
       <h1>登録が完了しました。</h1>
       <p>パスパラメーター：{userId}</p>
-      <p>登録結果：{RegResult}</p>
+      <div>登録結果：{RegResult}</div>
       <Register />
       <Link to="../../">戻る</Link>  
 
