eb2decdf1d03bd15dee56136cbabad659bbe0c16
haru <hrtkszk@gmail.com>
2022-05-29 14:59:10 +0900

    add inputs in registration.js

--- src/Registration.js
+++ src/Registration.js
@@ -6,7 +6,9 @@ import {
 import { useState } from 'react';
 
 export function Registration(){
-  const [RegResult, setRegResult] = useState("");
+  const [NickName, setNickName] = useState("");
+  const [Gender, setGender] = useState("");
+  const [Age, setAge] = useState("");
   const [initialized, setinitialized] = useState(false);
   let { userId } = useParams();
 
@@ -22,7 +24,7 @@ export function Registration(){
       fetch("../register.php",requestOptions1)
       .then((response)=> response.json())
       .then(result =>{
-        setRegResult(result.result)
+        setRegResult(result.result[0])
         console.log(result)
       })
       setinitialized(true)
@@ -37,7 +39,42 @@ export function Registration(){
       <div>登録結果：{RegResult}</div>
       <div><Register /></div>
       <Link to="../../">戻る</Link>  
+      <div>
+          <form onSubmit={e => submit(e)}>
+            <input
+              // pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+              // title="有効なメールアドレスを入力してください"
+              onChange={evt => {
+                // 本当は、この段階で入力制限を設けたい。ポップアップなどで入力できないことを示す？
+                //サーバー側でも入力制限を設けたい。
+                setNickName(evt.target.value.replace(/"/g, '”').replace(/#/g, '＃').replace(/\$/g, '＄').replace(/&/g, '＆').replace(/'/g, '’').replace(/\(/g,'（').replace(/\)/g,'）').replace(/\\/g, '＼').replace(/</g, '＜').replace(/>/g, '＞').replace(/\*/g, '＊').replace(/`/g, '｀').replace(/\|/g, '｜'))
+              }}
+              placeholder='ニックネーム'
+              required
+            /><br />
+            <div onChange={setGender(this.onChangeValue)}>
+              <input type="radio" value="Male" name="gender" /> 男性
+              <input type="radio" value="Female" name="gender" /> 女性
+              <input type="radio" value="Other" name="gender" /> LGBTQ
+            </div><br />
 
+
+              <input
+              type='number'
+              // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              // 特殊文字を弾く必要がある。
+              // title="8文字以上で、数字・小文字アルファベット・大文字アルファベットを含めてください"
+              onChange={evt => {
+                setAge(evt.target.value)
+              }}
+              placeholder='年齢'
+              required
+            /><br />
+            <button onClick={setTempRegister}>登録する</button>
+          </form>
+          <br />
+          <Link to="../">戻る</Link>
+        </div>
       </div>
   )
 }
