080511a7810cc7d458b30a0021688c85dc8fba67
haru <hrtkszk@gmail.com>
2022-06-04 16:29:49 +0900

    try to fix error in check_registry.py 16

--- public/check_registry.py
+++ public/check_registry.py
@@ -25,12 +25,12 @@ try:
     # 該当するUUIDのRegistrationStatusを1に変更する。
     cursor.execute(f"SELECT UUID, RegistrationStatus FROM {profiletable} WHERE UUID='{sys.argv[1]}'")
     checkExist = cursor.fetchall()
-    checkExist1 = cursor.fetchone()
+    # checkExist1 = cursor.fetchone()
     print(type(len(checkExist)))
     print(len(checkExist))
     print(type(0))
-    print(checkExist1)
-    print(type(checkExist1))
+    # print(checkExist1)
+    # print(type(checkExist1))
     # print(len(checkExist1))
     try:
         RegistrationStatus = cursor.fetchone()[1]
