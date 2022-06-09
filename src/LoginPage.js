e2429836f3487feb1be512536a3a0a9d5a8dd4fb
haru <hrtkszk@gmail.com>
2022-06-09 18:49:56 +0900

    try to update login logic to update user id

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -33,12 +33,14 @@ export function LoginPage() {
           // when they get to the protected page and click the back button, they
           // won't end up back on the login page, which is also really nice for the
           // user experience.
-          console.log(auth.user)
-          if (auth.user!=="") {
-            navigate(from, { replace: true });
-          } else {
-            console.log("Login Failed.")
-            navigate("", { replace: true });
+          () => {
+            console.log(auth.user)
+            if (auth.user!=="") {
+              navigate(from, { replace: true });
+            } else {
+              console.log("Login Failed.")
+              navigate("", { replace: true });
+            }
           }
         });
     }
