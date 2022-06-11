84e1d69e095a8b23663861a64e5b5b50946c0832
haru <hrtkszk@gmail.com>
2022-06-11 15:36:23 +0900

    try to fix login error

--- src/RequireAuth.js
+++ src/RequireAuth.js
@@ -12,12 +12,12 @@ export function RequireAuth({ children }) {
 
   console.log(auth.user)
   if (!auth.user) {
-    console.log(auth.user)
+    console.log(auth.user),
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    navigate("/")
+    () => navigate("/")
     // auth.signout(() => navigate("/"))
     // return <Navigate to="login" state={{ from: location }} replace />;
   } else {
