a877d52718a827628c2dde184dd44209e4cba7af
haru <hrtkszk@gmail.com>
2022-06-11 15:07:06 +0900

    try to fix login error by using navigate

--- src/RequireAuth.js
+++ src/RequireAuth.js
@@ -1,13 +1,15 @@
-import * as React from "react";
+// import * as React from "react";
 import {
-  useLocation,
-  Navigate
+  // useLocation,
+  useNavigate
 } from "react-router-dom";
 import { useAuth } from "./useAuth";
 
 export function RequireAuth({ children }) {
   let auth = useAuth();
-  let location = useLocation();
+  // let location = useLocation();
+  let navigate = useNavigate();
+
   console.log(auth.user)
   if (!auth.user) {
     console.log(auth.user)
@@ -15,7 +17,8 @@ export function RequireAuth({ children }) {
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    return <Navigate to="login" state={{ from: location }} replace />;
+    navigate("", { replace: true })
+    // return <Navigate to="login" state={{ from: location }} replace />;
   }
 
   return children;
