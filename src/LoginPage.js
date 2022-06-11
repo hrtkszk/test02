91f184144311a522820237a57dfcb3ce2601d5e6
haru <hrtkszk@gmail.com>
2022-06-11 14:14:31 +0900

    try shouldComponentUpdate() to update user id

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -33,7 +33,7 @@ export function LoginPage() {
         //   // when they get to the protected page and click the back button, they
         //   // won't end up back on the login page, which is also really nice for the
         //   // user experience.
-          this.forceUpdate()
+          shouldComponentUpdate()
           console.log(auth.user)
           if (auth.user!=="") {
             navigate(from, { replace: true });
