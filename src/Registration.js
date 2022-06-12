a5d3e6a8dfb65437aa4629d88a5c62f93842db7d
haru <hrtkszk@gmail.com>
2022-06-12 14:43:57 +0900

    try to fix error in registration.js

--- src/Registration.js
+++ src/Registration.js
@@ -55,9 +55,10 @@ export function Registration(){
             setinitialized(true)
             auth.setUser(userId)
             auth.setAuthStatus(true)
-            auth.setMessage("", () => {
+            auth.setMessage("")
+            {() => {
               navigate("../protected/", { replace: true })
-            })
+            }}
           })
           // setinitialized(true)
           // let username = userId
