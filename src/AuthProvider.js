54fd3275c971e93f93b10d4fc82bbaf97eb6f3f5
haru <hrtkszk@gmail.com>
2022-06-12 12:18:49 +0900

    try to find error in login func

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -22,7 +22,7 @@ export function AuthProvider({ children }) {
     fetch("../login.php",initialRequestOptions)
     .then((response)=> response.json())
     .then(result =>{
-      // console.log(result.result[0])
+      console.log(result.result)
       // console.log(result.result[1])
       // console.log(result.result(1))
       if (result.result[0]==="LS") {
