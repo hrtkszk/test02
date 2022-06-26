a6d68198351ab71a9fe4b1c086432671999bbf60
haru <hrtkszk@gmail.com>
2022-06-26 14:48:24 +0900

    try to fix error in login python

--- public/login.py
+++ public/login.py
@@ -73,8 +73,8 @@ if checkExist!=None:
         if latestpwd == sys.argv[2]:
             print("LS") # Login Success
             print(UUID)
-            if RegistrationStatus!=1:
-                print("RIC") # Registration InComplete
+            # if RegistrationStatus!=1:
+            #     print("RIC") # Registration InComplete
             print(RegistrationStatus)
         else:
             print("ICI") # InCorrect Input
