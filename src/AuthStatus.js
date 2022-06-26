3c3812f4c35b9dd81e7b351ff9dbff1fe0aa9c90
haru <hrtkszk@gmail.com>
2022-06-26 14:36:18 +0900

    update status message when not registered

--- src/AuthStatus.js
+++ src/AuthStatus.js
@@ -4,9 +4,11 @@ import { useAuth } from "./useAuth";
 export function AuthStatus() {
   let auth = useAuth();
 
-  if (auth.user && auth.AuthStatus) {
+  if (auth.user && auth.AuthStatus && auth.RegistrationStatus) {
     return <p>{auth.user}さんとしてログインしています</p>;
-  } else if (auth.Message!=="") {
+  } else if (auth.user && auth.AuthStatus && !auth.RegistrationStatus) {
+    return <p>基本情報が未登録です。基本情報を登録してください</p>;
+  }  else if (auth.Message!=="") {
     return <p>{auth.Message}</p>
   } else {
     return <p>ログインしてください</p>;
