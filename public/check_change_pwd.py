7f6710125611d4261abc0e6bb73118317f352b00
haru <hrtkszk@gmail.com>
2022-07-23 13:50:43 +0900

    try to find error in changepwd.js

--- public/check_change_pwd.py
+++ public/check_change_pwd.py
@@ -15,36 +15,39 @@ connection = MySQLdb.connect(
 
 pwdtable="PwdSettings"
 
-print("test")
+print("CPS")
 # OldPwdをチェック。
 # OldPwdがOKなら、NewPwdを追加する。
 
 # field name込みの場合はこっちを使う
 # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-cursor = connection.cursor()
 
-cursor.execute(f"SELECT * FROM {pwdtable} WHERE UUID='{sys.argv[1]}'")
 
-checkExist = cursor.fetchone()
-if checkExist==None:
-    print("UNE") # UUID Not Exist
-else:
-    # 最新パスワードの確認
-    cursor.execute(f"SELECT password FROM {pwdtable} WHERE UUID='{sys.argv[1]}' AND datetime = (SELECT MAX(datetime) FROM {pwdtable} WHERE UUID='{sys.argv[1]}')")
-    OldPwdDB = cursor.fetchone()[0]
-    if OldPwdDB == sys.argv[2]:
-        # 新パスワード登録
-        cursor.execute(f"INSERT `{pwdtable}` (`UUID`, `password`, `datetime`) VALUES ('{sys.argv[1]}', '{sys.argv[3]}', CURRENT_TIME)")
-        print("CPS") # Change Password Success
-        print(OldPwdDB)
-        # print(datetime_pwd)
-    else:
-        print("NS") # Not Success
-        # print(datetime_pwd)
-        print(OldPwdDB)
 
-# 保存を実行
-connection.commit()
+# cursor = connection.cursor()
 
-# 接続を閉じる
-connection.close()
+# cursor.execute(f"SELECT * FROM {pwdtable} WHERE UUID='{sys.argv[1]}'")
+
+# checkExist = cursor.fetchone()
+# if checkExist==None:
+#     print("UNE") # UUID Not Exist
+# else:
+#     # 最新パスワードの確認
+#     cursor.execute(f"SELECT password FROM {pwdtable} WHERE UUID='{sys.argv[1]}' AND datetime = (SELECT MAX(datetime) FROM {pwdtable} WHERE UUID='{sys.argv[1]}')")
+#     OldPwdDB = cursor.fetchone()[0]
+#     if OldPwdDB == sys.argv[2]:
+#         # 新パスワード登録
+#         cursor.execute(f"INSERT `{pwdtable}` (`UUID`, `password`, `datetime`) VALUES ('{sys.argv[1]}', '{sys.argv[3]}', CURRENT_TIME)")
+#         print("CPS") # Change Password Success
+#         print(OldPwdDB)
+#         # print(datetime_pwd)
+#     else:
+#         print("NS") # Not Success
+#         # print(datetime_pwd)
+#         print(OldPwdDB)
+
+# # 保存を実行
+# connection.commit()
+
+# # 接続を閉じる
+# connection.close()
