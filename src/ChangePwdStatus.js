14e8afd1ebe83406c299abfccbab674fe5ac0cd7
haru <hrtkszk@gmail.com>
2022-07-23 14:46:26 +0900

    try to show message in changepwd.js

--- src/ChangePwdStatus.js
+++ src/ChangePwdStatus.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 
 export function ChangePwdStatus(test) {
-  if (test=true) {
+  if (test==="true") {
     return <p>パスワードを変更しました</p>
-  } else if(test=false) {
+  } else if(test==="false") {
     return <p>パスワードを変更できませんでした</p>
   } else {
     return <p></p>
