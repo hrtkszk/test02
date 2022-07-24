0510d9034c636f2bab5ab17551fe82dd3d51fc75
haru <hrtkszk@gmail.com>
2022-07-24 11:39:56 +0900

    try to find error in changeemail.js 3

--- public/check_change_email.py
+++ public/check_change_email.py
@@ -20,36 +20,37 @@ emailtable="EmailSettings"
 
 # field name込みの場合はこっちを使う
 # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+print("test")
+# cursor = connection.cursor()
 
-cursor = connection.cursor()
+# # UUIDのチェック（外部からの変更防止）
 
-# UUIDのチェック（外部からの変更防止）
-cursor.execute(f"SELECT * FROM {emailtable} WHERE UUID='{sys.argv[1]}'")
-checkExist = cursor.fetchone()
-print(checkExist)
-if checkExist==None:
-    print("UNE") # UUID Not Exist
-else:
+# cursor.execute(f"SELECT * FROM {emailtable} WHERE UUID='{sys.argv[1]}'")
+# checkExist = cursor.fetchone()
+# print(checkExist)
+# if checkExist==None:
+#     print("UNE") # UUID Not Exist
+# else:
 
-    # DB全体で同じメールアドレスが存在するかの確認
-    cursor.execute(f"SELECT email FROM {emailtable} WHERE email='{sys.argv[2]}'")
-    SameEmailDB = cursor.fetchone()[0]
-    print(SameEmailDB)
+#     # DB全体で同じメールアドレスが存在するかの確認
+#     cursor.execute(f"SELECT email FROM {emailtable} WHERE email='{sys.argv[2]}'")
+#     SameEmailDB = cursor.fetchone()[0]
+#     print(SameEmailDB)
 
-    # 存在しない→変更する
-    if SameEmailDB == None:
-        cursor.execute(f"INSERT `{emailtable}` (`UUID`, `email`, `datetime`) VALUES ('{sys.argv[1]}', '{sys.argv[2]}', CURRENT_TIME)")
-        print("CES") # Change Email Success
-        print(SameEmailDB)
+#     # 存在しない→変更する
+#     if SameEmailDB == None:
+#         cursor.execute(f"INSERT `{emailtable}` (`UUID`, `email`, `datetime`) VALUES ('{sys.argv[1]}', '{sys.argv[2]}', CURRENT_TIME)")
+#         print("CES") # Change Email Success
+#         print(SameEmailDB)
 
-    # 存在する→変更しない
-    else:
-        print("NS") # Not Success
-        print(SameEmailDB)
-        # print(datetime_pwd)
+#     # 存在する→変更しない
+#     else:
+#         print("NS") # Not Success
+#         print(SameEmailDB)
+#         # print(datetime_pwd)
 
-# 保存を実行
-connection.commit()
+# # 保存を実行
+# connection.commit()
 
-# 接続を閉じる
-connection.close()
+# # 接続を閉じる
+# connection.close()
