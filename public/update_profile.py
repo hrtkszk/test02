fbdb4236bb89bd4cffc2612491306f386134aaaf
haru <hrtkszk@gmail.com>
2022-07-24 15:54:34 +0900

    try to find error in updateprofile.py

--- public/update_profile.py
+++ public/update_profile.py
@@ -28,45 +28,49 @@ checkExist = cursor.fetchone()
 # UUIDが存在しない→初めての追加？（それとも外部からのアタックか）
 if checkExist==None:
     print("test1")
-    cursor.execute(f" \
-        INSERT `{profileTable}` \
-        SET \
-            UUID='{sys.argv[1]}', \
-            Prefecture='{sys.argv[2]}', \
-            City='{sys.argv[3]}', \
-            Height='{sys.argv[4]}', \
-            Style='{sys.argv[5]}', \
-            Looks='{sys.argv[6]}', \
-            Cup='{sys.argv[7]}', \
-            BustSize='{sys.argv[8]}', \
-            WestSize='{sys.argv[9]}', \
-            HipSize='{sys.argv[10]}', \
-            BloodType='{sys.argv[11]}', \
-            Job='{sys.argv[12]}', \
-            EduBack='{sys.argv[13]}', \
-            BirthPrefecture='{sys.argv[14]}', \
-            Zodiac='{sys.argv[15]}', \
-            MarriageStatus='{sys.argv[16]}', \
-            Kids='{sys.argv[17]}', \
-            Tabacco='{sys.argv[18]}', \
-            Alchole='{sys.argv[19]}', \
-            Car='{sys.argv[20]}', \
-            Interest='{sys.argv[21]}', \
-            ProfilePicture='{sys.argv[22]}', \
-            ProfileMessage='{sys.argv[23]}', \
-            PreferedAge='{sys.argv[24]}', \
-            PreferedPersonality='{sys.argv[25]}', \
-            Personality='{sys.argv[26]}', \
-            SelfCute='{sys.argv[27]}', \
-            SelfSexy='{sys.argv[28]}', \
-            SelfKindness='{sys.argv[29]}', \
-            SelfSmartness='{sys.argv[30]}', \
-            SelfNeatness='{sys.argv[31]}', \
-            SelfFashionable='{sys.argv[32]}', \
-            SelfBrightness='{sys.argv[33]}', \
-            SelfElegance='{sys.argv[34]}' \
-    ")
-    print("UPS") # Update Profile Success
+    try:
+        cursor.execute(f" \
+            INSERT INTO `{profileTable}` \
+            SET \
+                UUID='{sys.argv[1]}', \
+                Prefecture='{sys.argv[2]}', \
+                City='{sys.argv[3]}', \
+                Height='{sys.argv[4]}', \
+                Style='{sys.argv[5]}', \
+                Looks='{sys.argv[6]}', \
+                Cup='{sys.argv[7]}', \
+                BustSize='{sys.argv[8]}', \
+                WestSize='{sys.argv[9]}', \
+                HipSize='{sys.argv[10]}', \
+                BloodType='{sys.argv[11]}', \
+                Job='{sys.argv[12]}', \
+                EduBack='{sys.argv[13]}', \
+                BirthPrefecture='{sys.argv[14]}', \
+                Zodiac='{sys.argv[15]}', \
+                MarriageStatus='{sys.argv[16]}', \
+                Kids='{sys.argv[17]}', \
+                Tabacco='{sys.argv[18]}', \
+                Alchole='{sys.argv[19]}', \
+                Car='{sys.argv[20]}', \
+                Interest='{sys.argv[21]}', \
+                ProfilePicture='{sys.argv[22]}', \
+                ProfileMessage='{sys.argv[23]}', \
+                PreferedAge='{sys.argv[24]}', \
+                PreferedPersonality='{sys.argv[25]}', \
+                Personality='{sys.argv[26]}', \
+                SelfCute='{sys.argv[27]}', \
+                SelfSexy='{sys.argv[28]}', \
+                SelfKindness='{sys.argv[29]}', \
+                SelfSmartness='{sys.argv[30]}', \
+                SelfNeatness='{sys.argv[31]}', \
+                SelfFashionable='{sys.argv[32]}', \
+                SelfBrightness='{sys.argv[33]}', \
+                SelfElegance='{sys.argv[34]}' \
+        ")
+    except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
+        print(e)
+    finally:
+        print("UPS") # Update Profile Success
 
 # UUIDが存在する場合→更新（変更ないところはそのままにしたいが・・・ロジックが必要）
 else:
