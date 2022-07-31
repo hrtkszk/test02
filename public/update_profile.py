2ce717162f80cbb26303ff6bb1967266a6bc077f
haru <hrtkszk@gmail.com>
2022-07-31 14:07:31 +0900

    try to find error in profilesetting

--- public/update_profile.py
+++ public/update_profile.py
@@ -31,17 +31,16 @@ if checkExist==None:
         cursor.execute(f" \
             INSERT INTO `{profileTable}` \
             SET \
-                UUID='{sys.argv[1]}', \
-                Prefecture='{sys.argv[2]}', \
-                City='{sys.argv[3]}', \
-                Hight='{sys.argv[4]}', \
-                Style='{sys.argv[5]}' \
+                UUID='{sys.argv[1]}' \
         ")
         print("UPS") # Update Profile Success
     except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
         print(e)
         print("UPNS") # Update Profile Not Success
-
+                # Prefecture='{sys.argv[2]}', \
+                # City='{sys.argv[3]}', \
+                # Hight='{sys.argv[4]}', \
+                # Style='{sys.argv[5]}' \
                 # Looks='{sys.argv[6]}', \
                 # Cup='{sys.argv[7]}', \
                 # BustSize='{sys.argv[8]}', \
