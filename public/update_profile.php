240204ca5fab605ceed3f6ce9030190c107c2d7e
haru <hrtkszk@gmail.com>
2022-08-01 08:53:33 +0900

    try to find and fix error in profilesetting

--- public/update_profile.php
+++ public/update_profile.php
@@ -4,12 +4,12 @@ header('Access-Control-Allow-Headers: Content-Type');
 $rest_json = file_get_contents("php://input"); // JSONでPOSTされたデータを取り出す
 $_POST = json_decode($rest_json, true); // JSON文字列をデコード
 if ($_POST['Prefecture'] =="") {
-  $_POST['Prefecture']="未設定"
+  $Prefecture="未設定"
 }
 
 $command_post="python3 update_profile.py " //pythonに引数を渡す
 .$_POST['UUID']." "
-.$_POST['Prefecture']." "
+.$Prefecture." "
 .$_POST['City']." "
 .$_POST['Height']." "
 .$_POST['Style']." "
