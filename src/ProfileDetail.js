b71941e9570598c9224dcc949759981d51ff41f9
haru <hrtkszk@gmail.com>
2022-08-01 11:45:33 +0900

    fix error in profiledetail

--- src/ProfileDetail.js
+++ src/ProfileDetail.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-// import {
-//   Link,
+import {
+  Link
 //   // Outlet
 //   useNavigate
-// } from "react-router-dom";
+} from "react-router-dom";
 import { useState } from 'react';
 import { useAuth } from "./useAuth";
 
