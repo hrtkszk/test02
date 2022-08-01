a3548d2d5f6d7e07314ce809d6e0b4c11a8b6641
haru <hrtkszk@gmail.com>
2022-08-01 10:13:16 +0900

    try to find and fix error in profilesetting

--- public/update_profile.php
+++ public/update_profile.php
@@ -3,9 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
 $rest_json = file_get_contents("php://input"); // JSONでPOSTされたデータを取り出す
 $_POST = json_decode($rest_json, true); // JSON文字列をデコード
-// echo $_POST['Prefecture'];
+
 if ($_POST['Prefecture']=="") {
-  $Prefecture="test";
+  $Prefecture="未設定";
 } else {
   $Prefecture=$_POST['Prefecture'];
 }
