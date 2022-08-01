67d858246f1e9d8a94897ed1505b9bcf113cfbb0
haru <hrtkszk@gmail.com>
2022-08-01 17:38:16 +0900

    update appearence in profile detail

--- src/ProfileDetail.js
+++ src/ProfileDetail.js
@@ -23,10 +23,6 @@ export function ProfileDetail() {
     fetch("../../get_profile.php",initialRequestOptions)
     .then((response)=> response.json())
     .then(result =>{
-      console.log(result.test)
-      console.log(result.test0)
-      console.log(result.test1)
-      console.log(result.result[0])
       setProfile(result.result[0])
     })
     setinitialized(true)
@@ -49,14 +45,140 @@ export function ProfileDetail() {
     return (
       <div>
         <h1>プロフィール</h1>
-        {Profile.UUID}
-        {/* <ul>
-          {Profile.map((ProfileDatum, i) => {
-            return <li key={ProfileDatum.UUID}> 
-                {ProfileDatum.Prefecture}
-            </li>
-          })}
-        </ul> */}
+        <ul>
+          <li>
+            <span className="dan">都道府県</span>
+            <span className="dan2">{Profile.Prefecture}</span>
+          </li>
+          <li>
+            <span className="dan">市町村</span>
+            <span className="dan2">{Profile.City}</span>
+          </li>
+          <li>
+            <span className="dan">身長</span>
+            <span className="dan2">{Profile.Height}</span>
+          </li>
+          <li>
+            <span className="dan">スタイル</span>
+            <span className="dan2">{Profile.Style}</span>
+          </li>
+          <li>
+            <span className="dan">ルックス</span>
+            <span className="dan2">{Profile.Looks}</span>
+          </li>
+          <li>
+            <span className="dan">カップ</span>
+            <span className="dan2">{Profile.Cup}</span>
+          </li>
+          <li>
+            <span className="dan">胸サイズ</span>
+            <span className="dan2">{Profile.BustSize}</span>
+          </li>
+          <li>
+            <span className="dan">腰サイズ</span>
+            <span className="dan2">{Profile.WestSize}</span>
+          </li>
+          <li>
+            <span className="dan">ヒップサイズ</span>
+            <span className="dan2">{Profile.HipSize}</span>
+          </li>
+          <li>
+            <span className="dan">血液型</span>
+            <span className="dan2">{Profile.BloodType}</span>
+          </li>
+          <li>
+            <span className="dan">職業</span>
+            <span className="dan2">{Profile.Job}</span>
+          </li>
+          <li>
+            <span className="dan">学歴</span>
+            <span className="dan2">{Profile.EduBack}</span>
+          </li>
+          <li>
+            <span className="dan">出身地</span>
+            <span className="dan2">{Profile.BirthPrefecture}</span>
+          </li>
+          <li>
+            <span className="dan">星座</span>
+            <span className="dan2">{Profile.Zodiac}</span>
+          </li>
+          <li>
+            <span className="dan">交際状況</span>
+            <span className="dan2">{Profile.MarriageStatus}</span>
+          </li>
+          <li>
+            <span className="dan">子供</span>
+            <span className="dan2">{Profile.Kids}</span>
+          </li>
+          <li>
+            <span className="dan">タバコ</span>
+            <span className="dan2">{Profile.Tabacco}</span>
+          </li>
+          <li>
+            <span className="dan">お酒</span>
+            <span className="dan2">{Profile.Alchole}</span>
+          </li>
+          <li>
+            <span className="dan">車</span>
+            <span className="dan2">{Profile.Car}</span>
+          </li>
+          <li>
+            <span className="dan">興味あること</span>
+            <span className="dan2">{Profile.Interest}</span>
+          </li>
+          <li>
+            <span className="dan">プロフィール写真</span>
+            <span className="dan2">{Profile.ProfilePicture}</span>
+          </li>
+          <li>
+            <span className="dan">メッセージ</span>
+            <span className="dan2">{Profile.ProfileMessage}</span>
+          </li>
+          <li>
+            <span className="dan">希望する年齢</span>
+            <span className="dan2">{Profile.PreferedAge}</span>
+          </li>
+          <li>
+            <span className="dan">希望する性格</span>
+            <span className="dan2">{Profile.PreferedPersonality}</span>
+          </li>
+          <li>
+            <span className="dan">性格</span>
+            <span className="dan2">{Profile.Personality}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(可愛さ)</span>
+            <span className="dan2">{Profile.SelfCute}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(セクシー)</span>
+            <span className="dan2">{Profile.SelfSexy}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(優しさ)</span>
+            <span className="dan2">{Profile.SelfKindness}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(賢さ)</span>
+            <span className="dan2">{Profile.SelfSmartness}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(清楚さ)</span>
+            <span className="dan2">{Profile.SelfNeatness}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(ファッション)</span>
+            <span className="dan2">{Profile.SelfFashionable}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(明るさ)</span>
+            <span className="dan2">{Profile.SelfBrightness}</span>
+          </li>
+          <li>
+            <span className="dan">自己評価(エレガンス)</span>
+            <span className="dan2">{Profile.SelfElegance}</span>
+          </li>
+        </ul>
         <div>
           <Link to="../">戻る</Link>
         </div>
