1d4ba12676a01059853b20a089a430c971c0afc0
haru <hrtkszk@gmail.com>
2022-08-02 18:44:28 +0900

    try to figureout how to add detailarea from json

--- src/ProfileDetail.js
+++ src/ProfileDetail.js
@@ -48,7 +48,7 @@ export function ProfileDetail() {
     //  profileTableにUUIDがあれば、プロフィールを表示＋設定ページボタンの表示
   } else {
     console.log(ProfileDB.Area[Profile.Prefecture])
-    console.log(ProfileDB.Area[Profile.Prefecture]["AreaName"])
+    // console.log(ProfileDB.Area[Profile.Prefecture]["AreaName"])
     return (
       <div>
         <h1>プロフィール</h1>
