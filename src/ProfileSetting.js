676c8ca4a9c9ea7950c5eabc2f5ae31214aff382
haru <hrtkszk@gmail.com>
2022-08-03 18:26:22 +0900

    try to figureout how to change preferedagerange

--- src/ProfileSetting.js
+++ src/ProfileSetting.js
@@ -249,7 +249,8 @@ export function ProfileSetting() {
   function changePreferedAgeRange1() {
     if (Number(PreferedAge1) > Number(PreferedAge2)) {
       for (let i = Number(PreferedAge2) + 1; i < 17; i++) {
-        delete latestPreferedAgeRange1.String(i)
+        j = String(i)
+        delete latestPreferedAgeRange1.j
       }
       console.log(latestPreferedAgeRange1)
       setPreferedAgeRange1(latestPreferedAgeRange1)
