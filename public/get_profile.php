96bf0c224b53571996c0267361f93d6c6f78fb6f
haru <hrtkszk@gmail.com>
2022-08-03 14:13:03 +0900

    try to figureout error in getprofile.php

--- public/get_profile.php
+++ public/get_profile.php
@@ -7,39 +7,47 @@ $_POST = json_decode($rest_json, true); // JSON文字列をデコード
 $command="python3 get_profile.py ".$_POST['UUID']; //pythonに引数を渡す
 exec($command,$output); //python実行と、返り数受け取り
 
-// // pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
-// $output[0]=trim($output[0],"\"['");
-// $output[0]=trim($output[0],"']\"");
-// $output0=explode("', '",$output[0]);
+// pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
+$output[0]=trim($output[0],"\"['");
+$output[0]=trim($output[0],"']\"");
+$output0=explode("', '",$output[0]);
 
-// $output2=array();
-// $it = 0;
+$output2=array();
+$it = 0;
 
-// // pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
-// foreach ($output as $value) {
-//     $value=trim($value,"\"(");
-//     $value=trim($value,")\"");
-//     $value=str_replace("'",'',$value);
-//     $value1=explode(", ",$value);
-//     $output1 = array_combine($output0,$value1);
-//     $output2 = $output2 + array("$it" => $output1);
-//     $it = $it + 1;
-// }
-// // 配列1行目の削除と、配列詰め
-// unset($output2[0]);
-// $output2 = array_values($output2);
+// pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
+if($output[1]!="None") {
+    foreach ($output as $value) {
+        $value=trim($value,"\"(");
+        $value=trim($value,")\"");
+        $value=str_replace("'",'',$value);
+        $value1=explode(", ",$value);
+        $output1 = array_combine($output0,$value1);
+        $output2 = $output2 + array("$it" => $output1);
+        $it = $it + 1;
+    }
+    // 配列1行目の削除と、配列詰め
+    unset($output2[0]);
+    $output2 = array_values($output2);
 
-//配列のJSON変換と、echoでのサーバーサイド出力。
-if(empty($_POST['UUID'])) {
-    echo json_encode(
-        [
-            "result" => $output,
-        ]
-    );
+    //配列のJSON変換と、echoでのサーバーサイド出力。
+    if(empty($_POST['UUID'])) {
+        echo json_encode(
+            [
+                "result" => $output,
+            ]
+        );
+    } else {
+        echo json_encode(
+            [
+                "result" => $output,
+            ]
+        );
+    }
 } else {
     echo json_encode(
         [
-            "result" => $output,
+            "result" => "",
         ]
     );
 }
