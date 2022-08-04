886fd6f2998f61429ee65c2caf617b09e6960d2a
haru <hrtkszk@gmail.com>
2022-08-04 17:10:03 +0900

    try to find error in boshulist

--- public/boshu_list.php
+++ public/boshu_list.php
@@ -7,38 +7,38 @@ $command_post="python3 boshu_list.py ".$_POST['UUID']; //pythonに引数を渡�
 exec($command_post, $output); //python実行と、返り数受け取り
 $test = $output;
 // pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
-$output[0]=trim($output[0],"\"['");
-$output[0]=trim($output[0],"']\"");
-$output0=explode("', '",$output[0]);
+// $output[0]=trim($output[0],"\"['");
+// $output[0]=trim($output[0],"']\"");
+// $output0=explode("', '",$output[0]);
 
-$output2=array();
-$it = 0;
+// $output2=array();
+// $it = 0;
 
-// pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
-foreach ($output as $value) {
-    $value=trim($value,"\"[");
-    $value=trim($value,"]\"");
-    $value=str_replace("'",'',$value);
-    $value1=explode(", ",$value);
-    $output1 = array_combine($output0,$value1);
-    $output2 = $output2 + array("$it" => $output1);
-    $it = $it + 1;
-}
-// 配列1行目の削除と、配列詰め
-unset($output2[0]);
-$output2 = array_values($output2);
+// // pythonからの返り数のうち、SQLの受け取りと、文字列から配列変換(pythonの残りの行全て)
+// foreach ($output as $value) {
+//     $value=trim($value,"\"[");
+//     $value=trim($value,"]\"");
+//     $value=str_replace("'",'',$value);
+//     $value1=explode(", ",$value);
+//     $output1 = array_combine($output0,$value1);
+//     $output2 = $output2 + array("$it" => $output1);
+//     $it = $it + 1;
+// }
+// // 配列1行目の削除と、配列詰め
+// unset($output2[0]);
+// $output2 = array_values($output2);
 
 //配列のJSON変換と、echoでのサーバーサイド出力。
 if(empty($_POST['UUID'])) {
     echo json_encode(
         [
-           "result" => $output,
+           "result" => $test,
         ]
     ); 
 } else {
     echo json_encode(
         [
-           "result" => $output,
+           "result" => $test,
         ]
     ); 
 }
