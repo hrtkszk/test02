f145f3dba7521330670dac45ae391f311617f9de
haru <hrtkszk@gmail.com>
2022-08-05 07:08:34 +0900

    try to fix error in registration

--- src/Registration.js
+++ src/Registration.js
@@ -52,7 +52,7 @@ export function Registration(){
               required
             /><br />
             <select
-              defaultValue="0"
+              defaultValue={0}
               onChange={evt => setGender(evt.target.value)}>
                 {Object.keys(ProfileDB.Gender).map(key => <option value={key}>{ProfileDB.Gender[key]}</option>)}
             </select><br />
