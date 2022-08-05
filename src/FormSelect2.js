6a347637855dbc7b07e7cd39a99e2f637cfdf55e
haru <hrtkszk@gmail.com>
2022-08-05 14:48:32 +0900

    try to figureout to use new formselect

--- src/FormSelect2.js
+++ src/FormSelect2.js
@@ -1,5 +1,7 @@
 const FormSelect2 = (props) => {
     let TempValue =""
+
+
     return (
         <>
             <span className="dan">{props.title}</span>
@@ -7,7 +9,10 @@ const FormSelect2 = (props) => {
                     <select
                         defaultValue = {props.defaultValue}
                         onChange={event => {
-                            TempValue = JSON.parse(JSON.stringify("{" + props.targetJSONName + ":" + event.target.value + "}"))
+                            TempValue = JSON.parse(JSON.stringify("{ " + props.targetJSONName + " : " + event.target.value + " }"))
+                            console.log(props.targetJSON)
+                            console.log(props.defaultValue)
+                            console.log(TempValue)
                             props.setValue({...props.targetJSON, TempValue })
                         }}>
                             {Object.keys(props.keyValue).map(key => <option value={key}>{props.keyValue[key]}</option>)}
