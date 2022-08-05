b3e1867a502e2242244532e9cc6bcb06efd27813
haru <hrtkszk@gmail.com>
2022-08-05 15:34:47 +0900

    try to figureout how to change new formselect

--- src/FormSelect2.js
+++ src/FormSelect2.js
@@ -7,7 +7,7 @@ const FormSelect2 = (props) => {
                     <select
                         defaultValue = {props.defaultValue}
                         onChange={event => {
-                            TempValue = JSON.parse(JSON.stringify(props.targetJSONName + ":" + event.target.value))
+                            TempValue = JSON.stringify("{\"" + props.targetJSONName + "\":\"" + event.target.value + "\"}")
                             console.log(props.targetJSON)
                             console.log(props.defaultValue)
                             console.log(TempValue)
