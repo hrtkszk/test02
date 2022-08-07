c04f2c2116cf8c2bed8aa2954fee86f0d36dcad8
haru <hrtkszk@gmail.com>
2022-08-07 14:35:12 +0900

    try to figout how to use multselect

--- src/FormMultiSelect.js
+++ src/FormMultiSelect.js
@@ -2,21 +2,17 @@ const FormMultiSelect = (props) => {
     return (
         <>
             <span className="dan">{props.title}</span>
-                <span className="dan2">
-                    <select
-                        multiple={true}
-                        defaultValue = {props.defaultValue}
-                        onChange = {event => {
-                            props.setValue([...props.defaultValue,
-                                {
-                                  id: event.target.value,
-                                  TF: "1",
-                                }
-                            ],)
-                            console.log(props.defaultValue)
-                            }}>
-                        {Object.keys(props.keyValue).map(key => <option value={key} disabled={props.keyValue[key]==="未設定"&& true}>{props.keyValue[key]}</option>)}
-                    </select>
+            <span className="dan2">
+                <select
+                    multiple={true}
+                    defaultValue = {props.defaultValue}
+                    onChange = {event => {
+                        props.setValue({...props.defaultValue, [event.target.value] : event.target})
+                        console.log(props.defaultValue)
+                        }}>
+                    {Object.keys(props.keyValue).map(key => <option value={key} disabled={props.keyValue[key]==="未設定"&& true}>{props.keyValue[key]}</option>)}
+                </select>
+                {/* {Object.keys(props.keyValue).map(key => <input type="checkbox" value={key}>{props.keyValue[key]}</input>)} */}
             </span>
         </>
     )    
