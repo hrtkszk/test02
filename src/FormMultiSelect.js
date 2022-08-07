f2cd20dda59a883a1917139cd69c0541e601f6e7
haru <hrtkszk@gmail.com>
2022-08-07 14:03:43 +0900

    try to figureout multiselect logic

--- src/FormMultiSelect.js
+++ src/FormMultiSelect.js
@@ -7,10 +7,14 @@ const FormMultiSelect = (props) => {
                         multiple={true}
                         defaultValue = {props.defaultValue}
                         onChange = {event => {
-                            props.setValue(event.target.value)
-                            console.log(event.target.value)
+                            props.setValue([...props.defaultValue,
+                                {
+                                  id: event.target.value,
+                                  TF: "1"
+                                }
+                            ])
                             }}>
-                                {Object.keys(props.keyValue).map(key => <option value={key} disabled={props.keyValue[key]==="未設定"&& true}>{props.keyValue[key]}</option>)}
+                        {Object.keys(props.keyValue).map(key => <option value={key} disabled={props.keyValue[key]==="未設定"&& true}>{props.keyValue[key]}</option>)}
                     </select>
             </span>
         </>
