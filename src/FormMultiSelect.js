56eb397ccd7baea72bb813f7d65ad10bc83d65cf
haru <hrtkszk@gmail.com>
2022-08-07 14:51:21 +0900

    try to use checkbox in multiselect

--- src/FormMultiSelect.js
+++ src/FormMultiSelect.js
@@ -1,18 +1,23 @@
 const FormMultiSelect = (props) => {
+    const handleOnChange = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
         <>
             <span className="dan">{props.title}</span>
             <span className="dan2">
-                <select
+                {/* <select
                     multiple={true}
                     defaultValue = {props.defaultValue}
                     onChange = {event => {
-                        props.setValue({...props.defaultValue, [event.target.value] : event.target})
+                        props.setValue({...props.defaultValue, [event.target.value] : event.target[event.target.value].select})
                         console.log(props.defaultValue)
                         }}>
                     {Object.keys(props.keyValue).map(key => <option value={key} disabled={props.keyValue[key]==="未設定"&& true}>{props.keyValue[key]}</option>)}
-                </select>
-                {/* {Object.keys(props.keyValue).map(key => <input type="checkbox" value={key}>{props.keyValue[key]}</input>)} */}
+                </select> */}
+                {Object.keys(props.keyValue).map(key => <input type="checkbox" value={key} onChange={handleOnChange}>{props.keyValue[key]}</input>)}
+                {/* checked={checkedItems[item.id]} */}
             </span>
         </>
     )    
