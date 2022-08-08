ffa79464dee758b11683d25c9c5acf1d06520177
haru <hrtkszk@gmail.com>
2022-08-08 14:09:53 +0900

    try to find error in multiselect2

--- src/FormMultiSelect2.js
+++ src/FormMultiSelect2.js
@@ -24,18 +24,19 @@ const FormMultiSelect = (props) => {
                                 id={props.title}
                                 checked={
                                     props.defaultValue[key] === 1 ? (
-                                        true
+                                        true,
+                                        console.log(props.title, ":", key, ":", props.defaultValue[key])
                                     ) : (
                                         props.defaultValue[key] === undefined ? (
                                             key === "0" ? (
-                                                true
+                                                true,
                                                 // "checked",
-                                                // console.log(props.title, ":", key, "=0 :",props.defaultValue[key], "=undefined")
+                                                console.log(props.title, ":", key, "=0 :",props.defaultValue[key], "=undefined")
                                             ) : (
                                                 false,
                                                 console.log(props.title, ":", key, "!=0 :",props.defaultValue[key], "=undefined")
                                             )
-                                         ) : (
+                                        ) : (
                                             false,
                                             console.log(props.title, ":", props.defaultValue[key], "!=undefined")
                                         )
