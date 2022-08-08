868ddb2f64c4b4dad98fd3852ae419a4b4cb89a8
haru <hrtkszk@gmail.com>
2022-08-09 07:17:30 +0900

    try to figout how to use checkbox in fms2

--- src/FormMultiSelect2.js
+++ src/FormMultiSelect2.js
@@ -1,14 +1,14 @@
 const FormMultiSelect = (props) => {
-    const handleOnChange = (event) => {
-        if (props.defaultValue[event.target.value] === 1) {
-            const copyDefaultValue = {...props.defaultValue}
-            delete copyDefaultValue[event.target.value]
-            props.setValue(copyDefaultValue)
-        } else {
-            props.setValue({...props.defaultValue, [event.target.value] : 1})
-        }
-        // console.log(event.target.value, ":", props.defaultValue)
-    }
+    // const handleOnChange = (event) => {
+    //     if (props.defaultValue[event.target.value] === 1) {
+    //         const copyDefaultValue = {...props.defaultValue}
+    //         delete copyDefaultValue[event.target.value]
+    //         props.setValue(copyDefaultValue)
+    //     } else {
+    //         props.setValue({...props.defaultValue, [event.target.value] : 1})
+    //     }
+    //     // console.log(event.target.value, ":", props.defaultValue)
+    // }
 
     return (
         <>
@@ -20,25 +20,35 @@ const FormMultiSelect = (props) => {
                             <input
                                 // value={key}
                                 type="checkbox"
-                                onChange={handleOnChange}
                                 id={props.title+key}
                                 checked={
-                                    true
-                                    // props.defaultValue[key] === 1 ? (
-                                    //     // console.log(props.title, ":", key, ":", props.defaultValue[key]),
-                                    //     true
-                                    // ) : (
-                                    //     props.defaultValue[key] === undefined && key === "0" ? (
-                                    //         // console.log(props.title, ":", key, "=0 :",props.defaultValue[key], "=undefined"),
-                                    //         true
-                                    //         // "checked",
-                                    //         // false,
-                                    //         // console.log(props.title, ":", key, "!=0 :",props.defaultValue[key], "=undefined")
-                                    //     ) : (
-                                    //         false
-                                    //         // console.log(props.title, ":", key, "!=0 or ", props.defaultValue[key], "!=undefined")
-                                    //     )
-                                    // )
+                                    props.defaultValue[key] === 1 ? (
+                                        // console.log(props.title, ":", key, ":", props.defaultValue[key]),
+                                        true
+                                    ) : (
+                                        props.defaultValue[key] === undefined && key === "0" ? (
+                                            // console.log(props.title, ":", key, "=0 :",props.defaultValue[key], "=undefined"),
+                                            true
+                                            // "checked",
+                                            // false,
+                                            // console.log(props.title, ":", key, "!=0 :",props.defaultValue[key], "=undefined")
+                                        ) : (
+                                            false
+                                            // console.log(props.title, ":", key, "!=0 or ", props.defaultValue[key], "!=undefined")
+                                        )
+                                    )
+                                }
+                                onChange={
+                                    event => {
+                                        if (props.defaultValue[event.target.value] === 1) {
+                                            const copyDefaultValue = {...props.defaultValue}
+                                            delete copyDefaultValue[event.target.value]
+                                            props.setValue(copyDefaultValue)
+                                        } else {
+                                            props.setValue({...props.defaultValue, [event.target.value] : 1})
+                                        }
+                                        // console.log(event.target.value, ":", props.defaultValue)
+                                    }
                                 }
                             />
                             {props.keyValue[key]}<br />
