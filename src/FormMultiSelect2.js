9514c1fc9c3874d4cd5329e20b13d273e91bd7bd
haru <hrtkszk@gmail.com>
2022-08-08 13:54:00 +0900

    try to figout error in multiselect2

--- src/FormMultiSelect2.js
+++ src/FormMultiSelect2.js
@@ -28,9 +28,16 @@ const FormMultiSelect = (props) => {
                                     ) : (
                                         props.defaultValue[key] === undefined ? (
                                             key === 0 ? (
-                                                true
-                                            ) : false
-                                         ) : false
+                                                true,
+                                                console.log(props.title, ":", key, "=0 :",props.defaultValue[key], "=undefined")
+                                            ) : (
+                                                false,
+                                                console.log(props.title, ":", key, "!=0 :",props.defaultValue[key], "=undefined")
+                                            )
+                                         ) : (
+                                            false,
+                                            console.log(props.title, ":", props.defaultValue[key], "!=undefined")
+                                        )
                                     )
                                 }
                             />
@@ -38,7 +45,7 @@ const FormMultiSelect = (props) => {
                         </label>
                     </>
                 ))}
-                {console.log(props.title, ":", Object.keys(props.defaultValue).length, ":", props.defaultValue)}
+                {/* {console.log(props.title, ":", Object.keys(props.defaultValue).length, ":", props.defaultValue)} */}
                 {/* checked={checkedItems[item.id]} */}
             </span>
         </>
