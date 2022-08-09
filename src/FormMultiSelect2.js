9ecb670a8c113edbf3c0096663797220d03478f8
haru <hrtkszk@gmail.com>
2022-08-09 22:51:26 +0900

    try to fix error in fms2

--- src/FormMultiSelect2.js
+++ src/FormMultiSelect2.js
@@ -52,7 +52,7 @@ const FormMultiSelect = (props) => {
 
     const defaultCheck = (key) => {
         console.log("typeofValue=", typeof(props.defaultValue), " ,key=", key)
-        if (props.defaultValue.length === 0) {
+        if (Object.keys(props.defaultValue).length === 0) {
             if (key === "0") {
                 props.setValue({...props.defaultValue, "0": 1})
                 setCheckJson({"0" : true})
