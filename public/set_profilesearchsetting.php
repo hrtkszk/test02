f27959f7c04ddb2d2a05caa3dfbd7bb52a8906f0
haru <hrtkszk@gmail.com>
2022-08-16 16:47:57 +0900

    try to figout error in set_pss

--- public/set_profilesearchsetting.php
+++ public/set_profilesearchsetting.php
@@ -267,18 +267,18 @@ $original = [
 // 		"str3": "I'm fine"
 // 	}
 // };
-$json = json_encode($original);
+$json = (string)json_encode($original);
 // json_decode($data, true);
 $data = json_decode($json, true);
 
 $command_post="python3 set_profilesearchsetting.py " //pythonに引数を渡す
 .$_POST['UUID']." "
-.(string)$json;
+.$json;
 exec($command_post, $output); //python実行と、返り数受け取り
 
 echo json_encode(
   [
-    "test1" => (string)$json,
+    "test1" => $json,
     "test2" => $data,
     "json_enc_PSGender" => json_encode($_POST['PSGender']),
     // "test" => json_decode($data, true),
