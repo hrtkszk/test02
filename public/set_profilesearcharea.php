5c984c2c6cf3079f149eaf2f5f138f87fcc5625d
haru <hrtkszk@gmail.com>
2022-08-19 17:41:56 +0900

    try to figout set_psa

--- public/set_profilesearcharea.php
+++ public/set_profilesearcharea.php
@@ -257,6 +257,8 @@ exec($command_post, $output); //python実行と、返り数受け取り
 echo json_encode(
   [
     "_POST" => $_POST,
+    "json_enc__POST" => json_encode($_POST),
+    "json_enc__POST_array" => json_encode(array($_POST)),
     "result" => $output,
     // "result" => "SPSSS",
   ]
