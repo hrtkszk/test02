3485210307fda4a0affa836ee3f0a7e3c8d5b885
haru <hrtkszk@gmail.com>
2022-08-20 13:00:57 +0900

    try to figout get_psa works

--- public/get_profilesearcharea.py
+++ public/get_profilesearcharea.py
@@ -23,22 +23,22 @@ try:
     cursor.execute(f"SELECT Area FROM {PSArea} WHERE UUID='{sys.argv[1]}'")
 
     # num_fields = len(cursor.description)
-    field_names = [i[0] for i in cursor.description]
-    print(field_names)
+    # field_names = [i[0] for i in cursor.description]
+    # print(field_names)
 
     # print(cursor.fetchone())
     for row in cursor:
         row1 = list()
         for item in row:
-            if str(type(item)) == "<class 'datetime.datetime'>":
-                # 時間を文字列に変換（php側の処理対策）
-                row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
-            elif str(type(item)) == "<class 'str'>":
-                # phpでの文字列から配列への変換時の誤動作防止用前処理
-                item = item.replace("'","’")
-                row1.append(item.replace(', ', '，'))
-            else:
-                row1.append(item)
+            # if str(type(item)) == "<class 'datetime.datetime'>":
+            #     # 時間を文字列に変換（php側の処理対策）
+            #     row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
+            # elif str(type(item)) == "<class 'str'>":
+            #     # phpでの文字列から配列への変換時の誤動作防止用前処理
+            #     item = item.replace("'","’")
+            #     row1.append(item.replace(', ', '，'))
+            # else:
+            row1.append(item)
         # printでのpythonからphpへの受け渡し
         print (row1)
 except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
