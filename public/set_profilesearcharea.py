3dd9c9eff2a77e9a40e9d812fbeff234fec57d47
haru <hrtkszk@gmail.com>
2022-08-20 11:38:17 +0900

    try to figout set_pss

--- public/set_profilesearcharea.py
+++ public/set_profilesearcharea.py
@@ -18,39 +18,26 @@ PSArea="PSArea"
 # field name込みの場合はこっちを使う
 # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
 
-# cursor = connection.cursor()
+cursor = connection.cursor()
 
-# try:
-    
-#     cursor.execute(f" \
-#         INSERT INTO `{PSArea}` \
-#         SET \
-#             UUID='{sys.argv[1]}', \
-#             PSGender='{data}' \
-#     ")
-#     print("SPSSS") # Set Profile Search Setting Success
-#     print(data)
-# except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
-#     print(e)
-#     print("SPSSNS") # Set Profile Search Setting Not Success
-#     print(data)
-
+try:
+    UUID_Area = ""
+    for Area in sys.argv[2:]:
+        cursor.execute(f" \
+            INSERT INTO `{PSArea}` \
+            SET \
+                UUID='{sys.argv[1]}', \
+                Area='{Area}' \
+        ")
+    print("SPSSS") # Set Profile Search Setting Success
+except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
+    print(e)
+    print("SPSSNS") # Set Profile Search Setting Not Success
 
 
-# # 保存を実行
-# connection.commit()
 
-# # 接続を閉じる
-# connection.close()
+# 保存を実行
+connection.commit()
 
-UUID_Area = ""
-for Area in sys.argv[2:]:
-    UUID_Area += f"({sys.argv[1]}, {Area}), "
-
-
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
-print(sys.argv[4])
-print(sys.argv[5])
-print(UUID_Area)
+# 接続を閉じる
+connection.close()
