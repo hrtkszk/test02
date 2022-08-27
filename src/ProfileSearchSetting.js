1efa9634b312d3dd67962a4a33c3883de491559c
haru <hrtkszk@gmail.com>
2022-08-27 10:46:24 +0900

    try to use indeterminate for checkcontrol in pss

--- src/ProfileSearchSetting.js
+++ src/ProfileSearchSetting.js
@@ -357,23 +357,24 @@ export function ProfileSearchSetting() {
                           }
                         }}
                         indeterminate={
-                          PSArea["secondRead"] === true ?
-                          (
-                            // Object.keys(PSArea).map(key => {
-                              PSArea[key1] === "undefined" ? (
-                                false
-                              ) : (
-                                PSArea[key1]
-                              )
-                              // if (parseInt(key1) < parseInt(key) &&  parseInt(key) < parseInt(key1) + 1000000) {
-                              //   return true
-                              // }
-                              // return <></>
-                              // })
-                          ) : (
-                              false,
-                              setPSArea({...PSArea, "secondRead" : true})
-                          )
+                          true
+                          // PSArea["secondRead"] === true ?
+                          // (
+                          //   // Object.keys(PSArea).map(key => {
+                          //     PSArea[key1] === "undefined" ? (
+                          //       false
+                          //     ) : (
+                          //       PSArea[key1]
+                          //     )
+                          //     // if (parseInt(key1) < parseInt(key) &&  parseInt(key) < parseInt(key1) + 1000000) {
+                          //     //   return true
+                          //     // }
+                          //     // return <></>
+                          //     // })
+                          // ) : (
+                          //     false,
+                          //     setPSArea({...PSArea, "secondRead" : true})
+                          // )
                         }
                         // checked={
                         //   PSArea["secondRead"] === true ?
