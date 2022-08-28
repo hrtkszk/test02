f4ff5d0c84cbbd11bb3777b7edeafcc3c3a466b4
haru <hrtkszk@gmail.com>
2022-08-28 14:17:02 +0900

    change true to 1 in fms2

--- src/FormMultiSelect2.js
+++ src/FormMultiSelect2.js
@@ -1,29 +1,29 @@
 const FormMultiSelect = (props) => {
 
-    if (props.defaultValue["secondRead"] === true) {
+    if (props.defaultValue["secondRead"] === 1) {
         if (Object.keys(props.defaultValue).length === 1) {
-            props.setValue({...props.defaultValue, "0" : true})
+            props.setValue({...props.defaultValue, "0" : 1})
         }
     }
 
     // 未設定がある場合
     const withUnsetSelectionHandle = (event) => {
         if (event.target.value === "0") {
-            if (props.defaultValue[event.target.value] === true) {
+            if (props.defaultValue[event.target.value] === 1) {
             } else {
-                props.setValue({"0" : true})
+                props.setValue({"0" : 1})
             }
         } else {
-            if (props.defaultValue[event.target.value] === true) {
+            if (props.defaultValue[event.target.value] === 1) {
                 const copyDefaultValue = {...props.defaultValue}
                 delete copyDefaultValue[event.target.value]
                 props.setValue(copyDefaultValue)
             } else {
                 const copyDefaultValue = {...props.defaultValue}
-                if (props.defaultValue["0"] === true) {
+                if (props.defaultValue["0"] === 1) {
                     delete copyDefaultValue["0"]
                 }
-                props.setValue({...copyDefaultValue, [event.target.value] : true})
+                props.setValue({...copyDefaultValue, [event.target.value] : 1})
             }
         }
         // console.log(props.defaultValue)
@@ -31,12 +31,12 @@ const FormMultiSelect = (props) => {
 
     // 未設定がない場合
     const withOutUnsetSelectionHandle = (event) => {
-        if (props.defaultValue[event.target.value] === true) {
+        if (props.defaultValue[event.target.value] === 1) {
             const copyDefaultValue = {...props.defaultValue}
             delete copyDefaultValue[event.target.value]
             props.setValue(copyDefaultValue)
         } else {
-            props.setValue({...props.defaultValue, [event.target.value] : true})
+            props.setValue({...props.defaultValue, [event.target.value] : 1})
         }
         // console.log(props.defaultValue)
     }
@@ -57,16 +57,16 @@ const FormMultiSelect = (props) => {
                                 }
                                 id={props.title+key}
                                 checked={
-                                    props.defaultValue["secondRead"] === true ?
+                                    props.defaultValue["secondRead"] === 1 ?
                                     (
                                         props.defaultValue[key] === "undefined" ? (
-                                            false
+                                            0
                                         ) : (
                                             props.defaultValue[key]
                                         )
                                     ) : (
-                                        false,
-                                        props.setValue({...props.defaultValue, "secondRead" : true})
+                                        0,
+                                        props.setValue({...props.defaultValue, "secondRead" : 1})
                                     )
                                 }
                             />
