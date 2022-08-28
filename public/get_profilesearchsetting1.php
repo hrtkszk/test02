1b767b89be6ec6fbe1bfd420b3369f10b20afbd5
haru <hrtkszk@gmail.com>
2022-08-28 15:15:49 +0900

    try to figout error in get_pss1

--- public/get_profilesearchsetting1.php
+++ public/get_profilesearchsetting1.php
@@ -6,7 +6,7 @@ $_POST = json_decode($rest_json, true); // JSON文字列をデコード
 
 $command="python3 get_profilesearchsetting1.py ".$_POST['UUID']; //pythonに引数を渡す
 exec($command,$output); //python実行と、返り数受け取り
-
+$test = $output;
 // pythonからの返り数のうち、SQLのヘッダーの受け取りと、文字列から配列変換(pythonの出力1行目)
 $output[0]=trim($output[0],"\"['");
 $output[0]=trim($output[0],"']\"");
@@ -35,12 +35,14 @@ if($output[1]!="None") {
         echo json_encode(
             [
                 "result" => $output2,
+                "test" => $test,
             ]
         );
     } else {
         echo json_encode(
             [
                 "result" => $output2,
+                "test" => $test,
             ]
         );
     }
--- src/ProfileSearchSetting1.js
+++ src/ProfileSearchSetting1.js
@@ -114,7 +114,7 @@ export function ProfileSearchSetting1() {
     .then((response) => response.json())
     .then(result => {
       if (result.result !== "PSSND") {
-        console.log(result.result[0])
+        console.log(result.test)
         setPSGender({...PSGender, PSGender0 : result.result[0].PSGender0})
         setPSGender({...PSGender, PSGender1 : result.result[0].PSGender1})
         setPSGender({...PSGender, PSGender2 : result.result[0].PSGender2})
