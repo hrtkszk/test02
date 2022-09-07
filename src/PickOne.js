a16893fd327ab81e936c6f291d81ec8570a10dfc
haru <hrtkszk@gmail.com>
2022-09-07 20:07:02 +0900

    try to update profiledetail to use profiletable1

--- src/PickOne.js
+++ src/PickOne.js
@@ -1,18 +1,21 @@
 const PickOne = (props) => {
     let str = ""
+    let display = ""
     Object.keys(props.keyValue).map(key => 
         <>
             {str = props.keyName.concat(key)}
             {console.log(props.DBValue[str])}
+            {props.DBValue[str] === 1 ? (
+                display=props.keyValue[key]
+                
+            ) : None}
         </>
     )
 
     return (
         <>
             <span className="dan">{props.title}</span>
-            <span className="dan2">
-
-            </span>
+            <span className="dan2">{display}</span>
         </>
     )    
 }
