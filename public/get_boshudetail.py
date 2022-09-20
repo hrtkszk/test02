6c5a6d03df32017e1ff4523b46231a6b91ab7a8b
haru <hrtkszk@gmail.com>
2022-09-20 20:25:57 +0900

    try to find error in boshudetail.py

--- public/get_boshudetail.py
+++ public/get_boshudetail.py
@@ -18,45 +18,45 @@ BoshuDB="BoshuDB"
 
 print(sys.argv[1])
 
-try:
-    RecieveData=json.loads(sys.argv[1])
-except (IndexError, TypeError, ValueError) as e:
-    print("Convert Str to JSON:", e)
+# try:
+#     RecieveData=json.loads(sys.argv[1])
+# except (IndexError, TypeError, ValueError) as e:
+#     print("Convert Str to JSON:", e)
 
 
-# BoshuID = " ".join(sys.argv[1:])
-# field name込みの場合はこっちを使う
-# cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-cursor = connection.cursor()
+# # BoshuID = " ".join(sys.argv[1:])
+# # field name込みの場合はこっちを使う
+# # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+# cursor = connection.cursor()
 
-try:
-    cursor.execute(f"SELECT * FROM `{BoshuDB}` WHERE BoshuID='{RecieveData.BoshuID}'")
+# try:
+#     cursor.execute(f"SELECT * FROM `{BoshuDB}` WHERE BoshuID='{RecieveData.BoshuID}'")
 
-    # num_fields = len(cursor.description)
-    field_names = [i[0] for i in cursor.description]
+#     # num_fields = len(cursor.description)
+#     field_names = [i[0] for i in cursor.description]
 
-    # print(cursor.fetchone())
-    for row in cursor:
-        row1 = list()
-        for item in row:
-            if str(type(item)) == "<class 'datetime.datetime'>":
-                # 時間を文字列に変換（php側の処理対策）
-                row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
-            elif str(type(item)) == "<class 'str'>":
-                # phpでの文字列から配列への変換時の誤動作防止用前処理
-                item = item.replace("'","’")
-                row1.append(item.replace(', ', '，'))
-            else:
-                row1.append(item)
-        # printでのpythonからphpへの受け渡し
-        DictBoshuDetail = dict(zip(field_names, row1))
-        JsonBoshuDetail = json.dumps(DictBoshuDetail)
-        print(JsonBoshuDetail)
-except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
-    print("SQL exec and Json output:",e)
+#     # print(cursor.fetchone())
+#     for row in cursor:
+#         row1 = list()
+#         for item in row:
+#             if str(type(item)) == "<class 'datetime.datetime'>":
+#                 # 時間を文字列に変換（php側の処理対策）
+#                 row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
+#             elif str(type(item)) == "<class 'str'>":
+#                 # phpでの文字列から配列への変換時の誤動作防止用前処理
+#                 item = item.replace("'","’")
+#                 row1.append(item.replace(', ', '，'))
+#             else:
+#                 row1.append(item)
+#         # printでのpythonからphpへの受け渡し
+#         DictBoshuDetail = dict(zip(field_names, row1))
+#         JsonBoshuDetail = json.dumps(DictBoshuDetail)
+#         print(JsonBoshuDetail)
+# except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
+#     print("SQL exec and Json output:",e)
 
-# 保存を実行
-connection.commit()
+# # 保存を実行
+# connection.commit()
 
-# 接続を閉じる
-connection.close()
+# # 接続を閉じる
+# connection.close()
