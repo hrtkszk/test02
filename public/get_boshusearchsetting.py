1bd3eee2af37d62afba29268a4c81373bd8b6461
haru <hrtkszk@gmail.com>
2022-09-24 14:00:51 +0900

    try to figout BSS work

--- public/get_boshusearchsetting.py
+++ public/get_boshusearchsetting.py
@@ -42,12 +42,12 @@ try:
     people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
             2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
 
-    people[3] = {}
+    # people[3] = {}
 
-    people[3]['name'] = 'Luna'
-    people[3]['age'] = '24'
-    people[3]['sex'] = 'Female'
-    people[3]['married'] = 'No'
+    # people[3]['name'] = 'Luna'
+    # people[3]['age'] = '24'
+    # people[3]['sex'] = 'Female'
+    people[2]['married'] = 'No'
 
     print(people)
 
