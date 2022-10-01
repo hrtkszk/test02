41319103fea98180b571a32941ea2a3d52fb1cc2
haru <hrtkszk@gmail.com>
2022-10-01 12:09:09 +0900

    try to figout FMS3 work

--- src/FormMultiSelect3.js
+++ src/FormMultiSelect3.js
@@ -5,8 +5,8 @@ const FormMultiSelect3 = (props) => {
     if (props.defaultValue[props.keyText] !== undefined) {
         SelectArray = props.defaultValue[props.keyText].split("_")
         console.log(SelectArray)
-        Object.keys(SelectArray).map(key, value => (
-            console.log(key, ":", value)
+        Object.keys(SelectArray).map(value => (
+            console.log(value)
         ))
         if (props.defaultValue[props.keyText + "secondRead"] === 1) {
             if (Object.keys(props.defaultValue[props.keyText]).length === 0) {
