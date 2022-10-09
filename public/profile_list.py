b568356a7ac8e10c2d2f92c26f282065400d2d9e
haru <hrtkszk@gmail.com>
2022-10-09 13:51:35 +0900

    try to figout PL work

--- public/profile_list.py
+++ public/profile_list.py
@@ -36,7 +36,7 @@ DictData = dict(zip(field_names, recieved_data))
 DictData1 = {}
 for k, v in DictData.items():
     if k[:2] == "PS":
-        if v != 0:
+        if v != 0 or v != "null":
             # PSを削除
             DictData1[k[2:]] = v
     else:
