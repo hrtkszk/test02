74a725684a6cffae2f3f3b312fbb8c3ba535c55e
haru <hrtkszk@gmail.com>
2022-10-09 14:02:26 +0900

    try to figout PL work

--- public/profile_list.py
+++ public/profile_list.py
@@ -34,7 +34,7 @@ try:
     DictData1 = {}
     for k, v in DictData.items():
         if k[:2] == "PS":
-            if v != 0 or v != "null":
+            if v != 0:
                 # PSを削除
                 DictData1[k[2:]] = v
         else:
