3ff543d780403d5b8f3ec8f268787ed0b6b1bfad
haru <hrtkszk@gmail.com>
2022-10-10 13:50:54 +0900

    try to figout PL work

--- public/profile_list.py
+++ public/profile_list.py
@@ -154,27 +154,28 @@ try:
         WHERE \
             {PSS_SQL} \
     ")
-    field_names = [i[0] for i in cursor.description]
+    print(cursor.fetchone())
+    # field_names = [i[0] for i in cursor.description]
 
-    for EachId in cursor:
-        row1 = list()
-        for item in EachId:
-            if str(type(item)) == "<class 'datetime.datetime'>":
-                # 時間を文字列に変換（php側の処理対策）
-                row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
-            elif str(type(item)) == "<class 'str'>":
-                # phpでの文字列から配列への変換時の誤動作防止用前処理
-                item = item.replace("'","’")
-                row1.append(item.replace(', ', '，'))
-            else:
-                row1.append(item)
-        # printでのpythonからphpへの受け渡し
-        DictProfile=dict(zip(field_names, row1))
-        DictProfile1 = {}
-        for k, v in DictProfile.items():
-            if v != 0:
-                DictProfile1[k] = v
-        print(json.dumps(DictProfile1))
+    # for EachId in cursor:
+    #     row1 = list()
+    #     for item in EachId:
+    #         if str(type(item)) == "<class 'datetime.datetime'>":
+    #             # 時間を文字列に変換（php側の処理対策）
+    #             row1.append(str(item.strftime("%Y/%m/%d %H:%M:%S")))
+    #         elif str(type(item)) == "<class 'str'>":
+    #             # phpでの文字列から配列への変換時の誤動作防止用前処理
+    #             item = item.replace("'","’")
+    #             row1.append(item.replace(', ', '，'))
+    #         else:
+    #             row1.append(item)
+    #     # printでのpythonからphpへの受け渡し
+    #     DictProfile=dict(zip(field_names, row1))
+    #     DictProfile1 = {}
+    #     for k, v in DictProfile.items():
+    #         if v != 0:
+    #             DictProfile1[k] = v
+    #     print(json.dumps(DictProfile1))
 except (MySQLdb.Error, MySQLdb.Warning, IndexError, TypeError) as e:
     print("Profile Search and Result:", e)
 
