162e5c6e2584cd79b1f4fa91147f5b62a1d9af10
haru <hrtkszk@gmail.com>
2022-10-15 13:36:59 +0900

    try to figout AgeRange work

--- src/ProfileSetting.js
+++ src/ProfileSetting.js
@@ -182,7 +182,7 @@ export function ProfileSetting() {
       headers:{'Content-Type': 'application/json'},
       body: JSON.stringify(s)
     }
-
+    console.log(requestOptions1)
     fetch("../../update_profile.php",requestOptions1)
     .then((response)=> response.json())
     .then(result =>{
