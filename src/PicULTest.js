38f2197df894ef847ae6258f07f748e5a70633cb
haru <hrtkszk@gmail.com>
2022-10-20 18:08:40 +0900

    try to figout picture upload logic

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import ImageUploader from 'react-images-upload';
 
-export function PicULTest(){
-  const [pictures, setPictures] = useState("");
-  console.log("Picture:", pictures)
+// export function PicULTest(){
+//   const [pictures, setPictures] = useState("");
+//   console.log("Picture:", pictures)
 
-  const onDrop=(pictureFiles)=>{
-    console.log("onDrop:", pictureFiles)
-    setPictures(pictures.concat(pictureFiles))
-  }
+//   const onDrop=(pictureFiles)=>{
+//     console.log("onDrop:", pictureFiles)
+//     setPictures(pictures.concat(pictureFiles))
+//   }
 
-  return(
-    <ImageUploader
-        withIcon={false}
-        buttonText='Choose images'
-        onChange={onDrop}
-        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-        maxFileSize={5242880}
-    />
-  )
+//   return(
+//     <ImageUploader
+//         withIcon={false}
+//         buttonText='Choose images'
+//         onChange={onDrop}
+//         imgExtension={['.jpg', '.gif', '.png', '.gif']}
+//         maxFileSize={5242880}
+//     />
+//   )
+// }
+
+import React from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+class App extends React.Component {
+ 
+    constructor(props) {
+        super(props);
+         this.state = { pictures: [] };
+         this.onDrop = this.onDrop.bind(this);
+    }
+    
+ 
+    onDrop(picture) {
+        console.log(picture)
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+        console.log(this.state.pictures)
+    }
+ 
+    render() {
+        return (
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+        );
+    }
 }
