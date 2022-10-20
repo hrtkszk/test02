b5ce6fdecccec478e683f33c70784a9559e32af9
haru <hrtkszk@gmail.com>
2022-10-20 18:36:28 +0900

    try to figout pic upload work

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -1,54 +1,53 @@
 import React from 'react';
-// import { useState } from 'react';
+import { useState } from 'react';
 import ImageUploader from 'react-images-upload';
 
-// export function PicULTest(){
-//   const [pictures, setPictures] = useState("");
+export function PicULTest(){
+  const [pictures, setPictures] = useState({pictures:[]});
 
-//   const onDrop=(pictureFiles)=>{
-//     console.log("onDrop:", pictureFiles)
-//     setPictures(pictureFiles)
-//   }
+  const onDrop=(event)=>{
+    console.log("onDrop:", event)
+    setPictures({pictures: pictures.pictures.concat(event)})
+  }
 
-//   console.log("Picture:", pictures)
-//   return(
-//     <ImageUploader
-//         withIcon={false}
-//         buttonText='Choose images'
-//         onChange={onDrop}
-//         imgExtension={['.jpg', '.gif', '.png', '.gif']}
-//         maxFileSize={5242880}
-//     />
-//   )
-// }
+  return(
+    <ImageUploader
+        withIcon={false}
+        buttonText='Choose images'
+        onChange={onDrop}
+        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        maxFileSize={5242880}
+    />
+  )
+}
 
 
 // import React from 'react';
 // import ImageUploader from 'react-images-upload';
  
-class PicULTest extends React.Component {
+// class PicULTest extends React.Component {
  
-    constructor(props) {
-        super(props);
-         this.state = { pictures: [] };
-         this.onDrop = this.onDrop.bind(this);
-    }
+//     constructor(props) {
+//         super(props);
+//          this.state = { pictures: [] };
+//          this.onDrop = this.onDrop.bind(this);
+//     }
  
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
-    }
+//     onDrop(picture) {
+//         this.setState({
+//             pictures: this.state.pictures.concat(picture),
+//         });
+//     }
  
-    render() {
-        return (
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-            />
-        );
-    }
-}
+//     render() {
+//         return (
+//             <ImageUploader
+//                 withIcon={true}
+//                 buttonText='Choose images'
+//                 onChange={this.onDrop}
+//                 imgExtension={['.jpg', '.gif', '.png', '.gif']}
+//                 maxFileSize={5242880}
+//             />
+//         );
+//     }
+// }
