b6fbe1bb06d71d4ff0d7e0fa864c5c153f184715
haru <hrtkszk@gmail.com>
2022-10-20 18:50:14 +0900

    try to figout pic upload work

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -1,25 +1,83 @@
-import React from 'react';
 import { useState } from 'react';
-import ImageUploader from 'react-images-upload';
 
-export function PicULTest(){
-  const [pictures, setPictures] = useState([]);
+function PicULTest() {
+  const [imgfile, uploadimg] = useState([])
+  console.log("Image FIles",imgfile);
 
-  const onDrop=(picture)=>{
-    console.log("onDrop:", picture)
-    setPictures(pictures.concat(picture))
+  const imgFilehandler = (e) => {
+    if (e.target.files.length !== 0) {
+      uploadimg(imgfile => [...imgfile, URL.createObjectURL(e.target.files[0])])
+    }
   }
 
-  return(
-    <ImageUploader
-        withIcon={true}
-        buttonText='Choose images'
-        onChange={onDrop}
-        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-        maxFileSize={5242880}
-    />
-  )
+  return (
+    <div className="App">
+      <div>
+        <center>
+          <h2>Upload</h2>
+          <input
+            type="file"
+            onChange={imgFilehandler}
+          />
+          <hr />
+          <h2>Preview</h2>
+          {imgfile.map((elem) => {
+            return <>
+              <span key={elem}>
+                <img src={elem} height="200" width="200" alt="med1" />
+              </span>
+            </>
+          })}
+        </center>
+      </div>
+    </div>
+  );
 }
+export default PicULTest;
+
+
+// const upload = (e) => {
+// 	console.warn(e.target.files)
+// 	const files = e.target.files
+// 	const formData = new FormData()
+// 	formData.append('img', files[0])
+// 	fetch('http://127.0.0.1:8000/api/store', {
+// 		method: 'POST',
+// 		body: formData,
+// 	}).then((resp) => {
+// 		resp.json().then((result) => {
+// 			console.warn(result)
+// 		})
+// 	})
+// }
+// return(<div>
+// 	<h1>Upload File in React js</h1>
+// 	<input type='file' onChange={(e) => upload(e)} name='img' />
+// </div>)
+
+
+// import React from 'react';
+// import { useState } from 'react';
+// import ImageUploader from 'react-images-upload';
+
+// export function PicULTest(){
+//   const [pictures, setPictures] = useState([]);
+
+//   const onChange=(event)=>{
+//     console.log("onChange:", event)
+//     setPictures(pictures.concat(event))
+//   }
+
+//   return(
+//     <ImageUploader
+//         withIcon={true}
+//         buttonText='Choose images'
+//         onChange={onChange}
+//         imgExtension={['.jpg', '.gif', '.png', '.gif']}
+//         maxFileSize={5242880}
+//     />
+//   )
+// }
 
 
 // import React from 'react';
