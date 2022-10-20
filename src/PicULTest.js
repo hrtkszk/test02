e147a607adc28eeadad47ee01d5ada7c683d2c87
haru <hrtkszk@gmail.com>
2022-10-20 17:56:16 +0900

    test picture upload

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -4,15 +4,15 @@ import ImageUploader from 'react-images-upload';
 
 export function PicULTest(){
   const [Picture, setPicture] = useState("");
-  console.log(Picture)
 
   const onDrop=(event)=>{
+    console.log(event)
     setPicture(event)
   }
 
   return(
     <ImageUploader
-        withIcon={true}
+        withIcon={false}
         buttonText='Choose images'
         onChange={onDrop}
         imgExtension={['.jpg', '.gif', '.png', '.gif']}
