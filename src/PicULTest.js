e37522fdc312d5b77af18beb6aea98a64b40cf1f
haru <hrtkszk@gmail.com>
2022-10-21 16:53:27 +0900

    try to figout pic upload work

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -19,13 +19,13 @@ export function PicULTest() {
 
   const submit = (event) => {
     event.preventDefault();
-    // const files = e.target.files
-    // console.log("submit:", e)
+
     console.log("imgfile:", imgfile)
+    const formData = new FormData()
+    formData.append('img', imgfile)
+    console.log(formData)
+    
     navigate("../")
-    // const formData = new FormData()
-    // formData.append('img', imgfile)
-    // console.log(formData)
     // fetch('http://127.0.0.1:8000/api/store', {
     //   method: 'POST',
     //   body: formData,
