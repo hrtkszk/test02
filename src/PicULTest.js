242cc7daa98750496ec606738f4b3c13672a7975
haru <hrtkszk@gmail.com>
2022-10-22 15:15:07 +0900

    try to figout pic upload work

--- src/PicULTest.js
+++ src/PicULTest.js
@@ -1,96 +1,100 @@
-// import * as React from "react";
-// import { useState } from 'react';
-// import {
-//   // Link,
-// //   // Outlet
-//   useNavigate
-// } from "react-router-dom";
+import * as React from "react";
+import { useState } from 'react';
+import {
+  // Link,
+//   // Outlet
+  useNavigate
+} from "react-router-dom";
 
 export function PicULTest() {
-//   const [imgfile, uploadimg] = useState("")
-//   // const [imgfile, uploadimg] = useState([])
-//   console.log("Image FIles",imgfile);
-//   let navigate = useNavigate();
+  const [imgfile, uploadimg] = useState("")
+  // const [imgfile, uploadimg] = useState([])
+  console.log("Image FIles",imgfile);
+  let navigate = useNavigate();
 
-//   // const imgFilehandler = (e) => {
-//   //   if (e.target.files.length !== 0) {
-//   //     uploadimg(imgfile => [...imgfile, URL.createObjectURL(e.target.files[0])])
-//   //   }
-//   // }
+  // const imgFilehandler = (e) => {
+  //   if (e.target.files.length !== 0) {
+  //     uploadimg(imgfile => [...imgfile, URL.createObjectURL(e.target.files[0])])
+  //   }
+  // }
 
-//   const submit = (event) => {
-//     event.preventDefault();
+  const submit = (event) => {
+    event.preventDefault();
 
-//     console.log("imgfile:", imgfile)
+    console.log("imgfile:", imgfile)
 
-//     const formData = new FormData()
-//     formData.set('img', imgfile);
-
-//     // formData.append('img', imgfile[0])
-//     console.log(formData)
-
-//     navigate("../")
-//     // fetch('http://127.0.0.1:8000/api/store', {
-//     //   method: 'POST',
-//     //   body: formData,
-//     // })
-//     // .then(resp => resp.json())
-//     // .then(result => console.log(result))
-//   }
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <center>
-//           <h2>Upload</h2>
-//           <form onSubmit={e => submit(e)}>
-//             <div>
-//               <label for="image_uploads">アップロードする画像を選択してください (PNG, JPG)</label>
-//               <input
-//                 type="file"
-//                 id="image_uploads"
-//                 name="image_uploads"
-//                 accept=".jpg, .jpeg, .png"
-//                 onChange={e => uploadimg(URL.createObjectURL(e.target.files[0]))}
-//                 // onChange={e => uploadimg(imgfile => [...imgfile, URL.createObjectURL(e.target.files[0])])}
-//               />
-//             </div>
-//             <br />
-//             <button>送信</button>
-//           </form>
-//           <hr />
-          
-//           <h2>Preview</h2>
-//           <img src={imgfile} height="200" width="200" alt="med1" />
-//           {/* {imgfile.map(elem => {
-//             return(<img src={elem} height="200" width="200" alt="med1" />)
-//           })} */}
-//         </center>
-//       </div>
-//     </div>
-//   );
-// }
-// export default PicULTest;
-
-
-  const upload = (e) => {
-    console.log(e.target.files)
-    const files = e.target.files
     const formData = new FormData()
-    formData.append('img', files[0])
+    // formData.set('img', imgfile);
+
+    formData.append('img', imgfile)
     console.log(formData)
-    fetch('../picULTest.php', {
+
+    const requestOptions1 ={
       method: 'POST',
-      body: formData,
-    })
+      enctype:"multipart/form-data",
+      body: formData
+    }
+
+    fetch('../piULTest.php',requestOptions1)
     .then(resp => resp.json())
     .then(result => console.log(result))
+
+    navigate("../")
   }
-  return(<div>
-    <h1>Upload File in React js</h1>
-    <input type='file' onChange={(e) => upload(e)} name='img' />
-  </div>)
+
+  return (
+    <div className="App">
+      <div>
+        <center>
+          <h2>Upload</h2>
+          <form onSubmit={e => submit(e)}>
+            <div>
+              {/* <label for="image_uploads">アップロードする画像を選択してください (PNG, JPG)</label> */}
+              <input
+                type="file"
+                id="img"
+                name="img"
+                accept=".jpg, .jpeg, .png"
+                onChange={e => uploadimg(URL.createObjectURL(e.target.files[0]))}
+                // onChange={e => uploadimg(imgfile => [...imgfile, URL.createObjectURL(e.target.files[0])])}
+              />
+            </div>
+            <br />
+            <button>送信</button>
+          </form>
+          <hr />
+          
+          <h2>Preview</h2>
+          <img src={imgfile} height="200" width="200" alt="med1" />
+          {/* {imgfile.map(elem => {
+            return(<img src={elem} height="200" width="200" alt="med1" />)
+          })} */}
+        </center>
+      </div>
+    </div>
+  );
 }
+export default PicULTest;
+
+
+//   const upload = (e) => {
+//     console.log(e.target.files)
+//     const files = e.target.files
+//     const formData = new FormData()
+//     formData.append('img', files[0])
+//     console.log(formData)
+//     fetch('../picULTest.php', {
+//       method: 'POST',
+//       body: formData,
+//     })
+//     .then(resp => resp.json())
+//     .then(result => console.log(result))
+//   }
+//   return(<div>
+//     <h1>Upload File in React js</h1>
+//     <input type='file' onChange={(e) => upload(e)} name='img' />
+//   </div>)
+// }
 
 // import React from 'react';
 // import { useState } from 'react';
