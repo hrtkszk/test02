9f25cbb3cc23c8f92548cfbb9c3deebdfb4769db
haru <hrtkszk@gmail.com>
2022-10-31 12:17:36 +0900

    try to figout pic upload work

--- public/picULTest.php
+++ public/picULTest.php
@@ -17,16 +17,20 @@ $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);//アップロ
 // $sql = "INSERT INTO images(name) VALUES (:image)";
 // $stmt = $dbh->prepare($sql);
 // $stmt->bindValue(':image', $image, PDO::PARAM_STR);
-if (!empty($_FILES['image']['name'])) {//ファイルが選択されていれば$imageにファイル名を代入
-    move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);//imagesディレクトリにファイル保存
-    // if (exif_imagetype($file)) {//画像ファイルかのチェック
-    //     $message = '画像をアップロードしました';
-    //     $stmt->execute();
-    // } else {
-    //     $message = '画像ファイルではありません';
-    // }
+echo $image;
+try {
+    if (!empty($_FILES['image']['name'])) {//ファイルが選択されていれば$imageにファイル名を代入
+        move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);//imagesディレクトリにファイル保存
+        // if (exif_imagetype($file)) {//画像ファイルかのチェック
+        //     $message = '画像をアップロードしました';
+        //     $stmt->execute();
+        // } else {
+        //     $message = '画像ファイルではありません';
+        // }
+    }
+} catch (Exception $e) {
+    echo $e;
 }
-
 // $input = file_get_contents('php://input');
 // echo var_dump($input);
 // preg_match('/WebKitFormBoundary(.*)$/', $input, $matches);
